b4ffa4b31ea1271b26d414b49ca269f276834ff7
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-03-08 12:34:16 -0300

    refactoring: divide the logic in multiple files

--- Src/Kernel/Boot/boot_stub.asm
+++ Src/Kernel/Boot/boot_stub.asm
@@ -1,38 +1,69 @@
-section .bss 
-align 16
-stack_bottom:
-  resb 16384 ; 16 KiB   TODO:Increase the value to 640KiB 
-stack_top:
-
-section .text 
-extern kernel_main
+section .text
+bits 32
 global _start
 
-_start:
-  ; Clean the screen
+extern kernel_main
+extern stack_top
+
+extern check_cpuid
+extern check_long_mode
+
+extern setup_page_tables
+extern enable_paging
+
+extern gdt64.code_segment
+extern gdt64.pointer
+
+
+clean_screen:
   mov edi, 0xb8000
   mov eax, 0x0720
-  mov ecx, 80*25
+  mov ecx, 80 * 25
   rep stosw
-  ; Jump cursor to initial position
-  xor eax, eax
-  mov dx, 0x3D4
+
+  mov dx, 0x3d4
   mov al, 0x0f
   out dx, al
   mov dx, 0x3D5
+  mov al, 0x00
   out dx, al
 
-  mov dx, 0x3D4
+  mov dx, 0x3d4
   mov al, 0x0E
-  out dx, al 
+  out dx, al
   mov dx, 0x3D5
+  mov al, 0x00
   out dx, al
 
-  ; Configure the kernel stack
+_start:
+  call clean_screen
+ 
   mov esp, stack_top
   mov ebp, 0
+ 
+  call check_cpuid
+  call check_long_mode
+
+  call setup_page_tables
+  call enable_paging
+
+  lgdt [gdt64.pointer]
+  jmp gdt64.code_segment:long_mode_start
+
+.hang:
+  hlt
+  jmp .hang
+
+.end:
+
+section .text
+bits 64
+long_mode_start:
+  mov ax, 0
+  mov ss, ax
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
   call kernel_main
 
-.hang: hlt
-  jmp .hang
-.end:
