25270a064ea23f6bd2c845ed18d7462c34899951
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-03-09 22:21:58 -0300

    feat: call init_idt in main

--- Src/Kernel/Init/main.c
+++ Src/Kernel/Init/main.c
@@ -1,8 +1,10 @@
 #include "../../../Include/Kernel/Descriptor/gdt.h"
+#include "../../../Include/Kernel/Descriptor/idt.h"
 #include "../../../Include/Kernel/Driver/vga_buffer.h"
 
 void kmain() {
   clear_screen();
   init_gdt();
+  init_idt();
   print_str("[OK]: There is the latest point in the kernel");
 }
