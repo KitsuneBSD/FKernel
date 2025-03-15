d1b3d92b935a258b71f2da5968f140c453962493
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-03-15 02:11:42 -0300

    ref: decrease the size of more common exceptions

--- Src/Kernel/Arch/Descriptor/exception_handler.c
+++ Src/Kernel/Arch/Descriptor/exception_handler.c
@@ -27,35 +27,17 @@ void panic(const char *segpoint, register_t *regs) {
   print_str(segpoint);
   print_str("\n");
 
+  log_registers(regs);
+
   while (1) {
     asm("cli; hlt");
   }
 }
 
-void generic_handler(register_t *regs) {
-  print_str("[Exception]: Generic Handler\n");
-  log_registers(regs);
+void generic_handler(register_t *regs) { panic("Unhandled exception", regs); }
 
-  panic("Unhandle exception", regs);
-}
-
-void invalid_opcode_handler(register_t *regs) {
-  print_str("[Exception] Invalid Opcode\n");
-  log_registers(regs);
-
-  uint8_t *opcode = (uint8_t *)regs->rip;
-
-  if (*opcode == 0xFF) {
-    print_str("Complex opcode detected, cannot safely skip.\n");
-    panic("Unhandled invalid opcode", regs);
-  }
-
-  regs->rip += 1;
-}
+void invalid_opcode_handler(register_t *regs) { panic("Invalid Opcode", regs); }
 
 void division_by_zero_handler(register_t *regs) {
-  print_str("[Exception] Division By Zero\n");
-  log_registers(regs);
-
   panic("Division By Zero", regs);
 }
