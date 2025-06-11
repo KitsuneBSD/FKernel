89277854968c529e9cfff7c86ab0f26da87cb806
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-11 08:17:54 -0300

    build(xmake): configure xmake to more expressive compile

--- xmake.lua
+++ xmake.lua
@@ -2,25 +2,7 @@ add_rules("mode.debug", "mode.release")
 set_policy("check.auto_ignore_flags", false)
 
 set_targetdir("build")
-set_objectdir("build/objs")
 
-set_languages("c++23")
-
-local clang_flags = {
-	"-g",
-	"-fno-threadsafe-statics",
-	"-fno-exceptions",
-	"-fno-rtti",
-	"-fno-stack-protector",
-}
-local nasm_flags = {
-	"-f elf64",
-	"-g",
-}
-
-local lld_flags = {
-	"-T Config/linker.ld",
-}
 toolchain("FKernel_Compiling")
 set_kind("standalone")
 set_toolset("cc", "clang")
@@ -29,31 +11,60 @@ set_toolset("ld", "ld.lld")
 set_toolset("as", "nasm")
 toolchain_end()
 
+-- NOTE: This flags isn't used in default xmake so, we need enforcing them
+local cxxflags_osdev = {
+	"-ffreestanding",
+	"-fno-threadsafe-statics",
+	"-fno-exceptions",
+	"-fno-rtti",
+	"-fno-stack-protector",
+}
+
+-- NOTE: We need enforcing the elf binary first
+local nasm_flags = {
+	"-f elf64",
+}
+
+-- NOTE: Enforcing the use of Config/Linker.ld file
+local linker_flags = {
+	"-T Config/linker.ld",
+}
+
 target("FKernel")
 set_kind("binary")
 set_default(true)
 set_filename("FKernel.bin")
 
-set_warnings(all)
+set_warnings("everything")
 
-add_cxflags(clang_flags)
+if is_mode("release") then
+	set_optimize("faster")
+	set_symbols("hidden")
+end
+
+if is_mode("debug") then
+	set_optimize("none")
+	set_symbols("debug")
+	add_defines("FKERNEL_DEBUG")
+end
+
+add_cxxflags(cxxflags_osdev)
 add_asflags(nasm_flags)
-add_ldflags(lld_flags)
+add_ldflags(linker_flags)
+
+add_files("Src/Kernel/Boot/**.cpp")
+add_files("Src/Kernel/Boot/**.asm")
 
 before_build(function(target)
-	os.execv("bash Meta/run_cppcheck.sh")
+	os.exec("bash Meta/run_cppcheck.sh")
 end)
 
 after_link(function(target)
-	os.execv("bash Meta/mounting_mockos.sh")
+	os.exec("bash Meta/mounting_mockos.sh")
 end)
 
 on_run(function(target)
-	os.execv("bash Meta/run_mockOS.sh")
-	--os.execv("bash Meta/run_kernel.sh")
+	os.exec("bash Meta/run_mockos.sh")
 end)
 
-add_includedirs("Include", "Include/Kernel", "Include/Lib")
-add_files("Src/Kernel/**.asm")
-add_files("Src/Kernel/**.cpp")
-add_files("Src/Lib/**.cpp")
+target_end()
