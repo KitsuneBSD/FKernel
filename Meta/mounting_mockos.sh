5655a5a7b51e2ddcb7cff5de32290ce65698a1be
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-11 08:18:20 -0300

    feat(scripts): update the running scripts

--- Meta/mounting_mockos.sh
+++ Meta/mounting_mockos.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
 print_error() {
-  echo -e "\e[41m\e[97mError: $1\e[0m"
+	echo -e "\e[41m\e[97mError: $1\e[0m"
 }
 
 if ! command -v grub-mkrescue &>/dev/null; then
-  print_error "grub-mkrescue not found. Please install grub-mkrescue before running this script."
-  exit 1
+	print_error "grub-mkrescue not found. Please install grub-mkrescue before running this script."
+	exit 1
 fi
 
 grub_directory="build/mockos/boot/grub"
@@ -14,9 +14,9 @@ grub_config="Config/grub.cfg"
 kernel_binary="build/FKernel.bin"
 
 if [ ! -d "$grub_directory" ]; then
-  mkdir -p "$grub_directory"
+	mkdir -p "$grub_directory"
 else
-  rm -rf "$grub_directory" && mkdir -p "$grub_directory"
+	rm -rf "$grub_directory" && mkdir -p "$grub_directory"
 fi
 
 cp "$grub_config" "$grub_directory"
