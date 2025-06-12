af7e33c09eb305f1cade4580648c732591f888f5
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-12 08:07:34 -0300

    feat(log function): instead use a VgaBuffer driver we can call a abstraction called 'log'

--- Include/LibFK/Log.h
+++ Include/LibFK/Log.h
@@ -3,19 +3,24 @@
 #include <Driver/Vga_Buffer.hpp>
 
 enum class LogLevel : uint8_t {
-  WARN = 1,
-  INFO = 0,
-  ERROR = 2,
+  INFO = 1,
+  WARN = 2,
+  ERROR = 3,
 };
 
 class Logger {
 private:
   static LogLevel currentLevel;
-
   static const char *LevelToString(LogLevel level);
   static vga::Color LevelToColor(LogLevel level);
 
 public:
+  static Logger &Instance();
   static void SetLevel(LogLevel level);
-  static void Log(LogLevel level, const char *message);
+
+  void Log(LogLevel level, const char *message) const;
 };
+
+inline void Log(LogLevel level, const char *message) {
+  Logger::Instance().Log(level, message);
+}
--- Src/LibFK/Log.cpp
+++ Src/LibFK/Log.cpp
@@ -5,22 +5,9 @@ using namespace vga;
 
 LogLevel Logger::currentLevel = LogLevel::INFO;
 
-void Logger::SetLevel(LogLevel level) { currentLevel = level; }
-
-void Logger::Log(LogLevel level, const char *message) {
-  if (static_cast<uint8_t>(level) < static_cast<uint8_t>(currentLevel)) {
-    return; // Filter out logs below the current level
-  }
-
-  vga::Color color = LevelToColor(level);
-  const char *levelStr = LevelToString(level);
-
-  console.set_color(color, vga::Color::Black);
-  console.write("[");
-  console.write(levelStr);
-  console.write("] ");
-  console.write(message);
-  console.write("\n");
+Logger &Logger::Instance() {
+  static Logger instance;
+  return instance;
 }
 
 const char *Logger::LevelToString(LogLevel level) {
@@ -39,12 +26,25 @@ const char *Logger::LevelToString(LogLevel level) {
 vga::Color Logger::LevelToColor(LogLevel level) {
   switch (level) {
   case LogLevel::INFO:
-    return vga::Color::LightGreen;
+    return Color::LightGreen;
   case LogLevel::WARN:
-    return vga::Color::Yellow;
+    return Color::Yellow;
   case LogLevel::ERROR:
-    return vga::Color::Red;
+    return Color::LightRed;
   default:
-    return vga::Color::White;
+    return Color::White;
   }
+}
+
+void Logger::SetLevel(LogLevel level) { currentLevel = level; }
+
+void Logger::Log(LogLevel level, const char *message) const {
+  if (level < currentLevel)
+    return;
+
+  console.set_color(LevelToColor(level), Color::Black);
+  console.write(LevelToString(level));
+  console.write(": ");
+  console.write(message);
+  console.write("\n");
 }
