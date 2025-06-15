4af85a2e77fb0cb5093fadfb6db24478d769e99c
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-15 11:52:16 -0300

    feat(multiboot2.h): enforce the use of static_assert to fit properly the size in x86_64

--- Include/Kernel/Boot/multiboot2.h
+++ Include/Kernel/Boot/multiboot2.h
@@ -4,21 +4,15 @@
 #include <LibC/stdint.h>
 
 namespace multiboot2 {
+constexpr uint32_t HEADER_ALIGN = 8;
+constexpr uint32_t HEADER_MAGIC = 0xe85250d6;
+constexpr uint32_t BOOTLOADER_MAGIC = 0x36d76289;
 
-using u8 = uint8_t;
-using u16 = uint16_t;
-using u32 = uint32_t;
-using u64 = uint64_t;
+constexpr uint32_t MOD_ALIGN = 0x00001000;
+constexpr uint32_t INFO_ALIGN = 0x00000008;
+constexpr uint32_t TAG_ALIGN = 8;
 
-constexpr u32 HEADER_ALIGN = 8;
-constexpr u32 HEADER_MAGIC = 0xe85250d6;
-constexpr u32 BOOTLOADER_MAGIC = 0x36d76289;
-
-constexpr u32 MOD_ALIGN = 0x00001000;
-constexpr u32 INFO_ALIGN = 0x00000008;
-constexpr u32 TAG_ALIGN = 8;
-
-enum class TagType : u32 {
+enum class TagType : uint32_t {
   End = 0,
   Cmdline = 1,
   BootLoaderName = 2,
@@ -43,7 +37,7 @@ enum class TagType : u32 {
   LoadBaseAddr = 21
 };
 
-enum class HeaderTagType : u32 {
+enum class HeaderTagType : uint32_t {
   End = 0,
   InformationRequest = 1,
   Address = 2,
@@ -57,21 +51,27 @@ enum class HeaderTagType : u32 {
   Relocatable = 10
 };
 
-enum class ConsoleFlags : u32 { ConsoleRequired = 1, EGATextSupported = 2 };
+enum class ConsoleFlags : uint32_t {
+  ConsoleRequired = 1,
+  EGATextSupported = 2
+};
 
-enum class Architecture : u32 { I386 = 0, MIPS32 = 4 };
+enum class Architecture : uint32_t { I386 = 0, MIPS32 = 4 };
 
-enum class LoadPreference : u32 { None = 0, Low = 1, High = 2 };
+enum class LoadPreference : uint32_t { None = 0, Low = 1, High = 2 };
 
 struct alignas(TAG_ALIGN) Tag {
   TagType type;
-  u32 size;
+  uint32_t size;
 
   const Tag *next() const noexcept {
     return reinterpret_cast<const Tag *>(
         reinterpret_cast<const uint8_t *>(this) + ((size + 7) & ~7u));
   }
 };
+
+static_assert(sizeof(Tag) == 8, "Tag struct must be exactly 8 bytes");
+static_assert(alignof(Tag) == 8, "Tag struct must be aligned to 8 bytes");
 
 struct alignas(TAG_ALIGN) TagString {
   Tag tag;
@@ -83,10 +83,12 @@ struct alignas(TAG_ALIGN) TagString {
   }
 };
 
+static_assert(alignof(TagString) == 8, "TagString must be aligned to 8 bytes");
+
 struct alignas(TAG_ALIGN) TagModule {
   Tag tag;
-  u32 mod_start;
-  u32 mod_end;
+  uint32_t mod_start;
+  uint32_t mod_end;
   char cmdline[0];
 
   [[nodiscard]]
@@ -95,9 +97,11 @@ struct alignas(TAG_ALIGN) TagModule {
   }
 };
 
+static_assert(alignof(TagModule) == 8, "TagModule must be aligned to 8 bytes");
+
 struct alignas(TAG_ALIGN) Info {
-  u32 total_size;
-  u32 reserved;
+  uint32_t total_size;
+  uint32_t reserved;
   Tag tags[0];
 
   [[nodiscard]]
@@ -105,31 +109,34 @@ struct alignas(TAG_ALIGN) Info {
     return tags;
   }
 };
+static_assert(sizeof(Info) == 8, "Info struct must be exactly 8 bytes");
+static_assert(alignof(Info) == 8, "Info struct must be aligned to 8 bytes");
 
 struct alignas(TAG_ALIGN) TagMemoryMap : Tag {
   struct Entry {
-    u64 base_addr;
-    u64 length;
-    u32 type;
-    u32 reserved;
+    uint64_t base_addr;
+    uint64_t length;
+    uint32_t type;
+    uint32_t reserved;
   };
+  static_assert(sizeof(Entry) == 24,
+                "TagMemoryMap::Entry must be exactly 24 bytes");
+  static_assert(alignof(Entry) == 8,
+                "TagMemoryMap::Entry must be aligned to 8 bytes");
 
-  u32 entry_size;
-  u32 entry_version;
+  uint32_t entry_size;
+  uint32_t entry_version;
   Entry entries[0];
 
   const Entry *begin() const noexcept { return entries; }
 
   const Entry *end() const noexcept {
     size_t entries_size = size - sizeof(TagMemoryMap);
-
     size_t count = entries_size / entry_size;
-
     return entries + count;
   }
 };
+static_assert(alignof(TagMemoryMap) == 8,
+              "TagMemoryMap must be aligned to 8 bytes");
 
-static_assert(sizeof(Tag) == 8);
-static_assert(alignof(Tag) == 8);
-static_assert(alignof(Info) == 8);
 }; // namespace multiboot2
