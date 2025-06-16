3b329132889ae0f90646931036ebe5f0ce12ec8c
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-16 07:32:26 -0300

    refactor(idt): turn the idt more clean code

--- Src/Kernel/Arch/x86_64/segments/idt.cpp
+++ Src/Kernel/Arch/x86_64/segments/idt.cpp
@@ -1,3 +1,4 @@
+#include <Kernel/Arch/x86_64/global_exception_handler.h>
 #include <Kernel/Arch/x86_64/idt.h>
 #include <Kernel/Arch/x86_64/stack_size.h>
 #include <LibFK/Log.h>
@@ -5,7 +6,8 @@
 alignas(0x10) IDTEntry idt[IDT_ENTRIES];
 IDTPointer idtp;
 
-void set_idt_entry(int vector, void (*handler)(), uint8_t ist, uint8_t flags) {
+void set_idt_entry(size_t vector, void (*handler)(), uint8_t ist,
+                   uint8_t flags) {
   uint64_t addr = reinterpret_cast<uint64_t>(handler);
   idt[vector].offset_low = addr & 0xFFFF;
   idt[vector].selector = 0x08; // Kernel code segment
@@ -16,19 +18,28 @@ void set_idt_entry(int vector, void (*handler)(), uint8_t ist, uint8_t flags) {
   idt[vector].zero = 0;
 }
 
-void init_idt() {
-  Logf(LogLevel::INFO, "Initializing Interrupt Descriptor Table (IDT)...");
+void init_idt_entries() {
+  Logf(LogLevel::INFO, "Populating IDT with default ISR handlers...");
 
-  for (int i = 0; i < IDT_ENTRIES; ++i) {
+  for (size_t i = 0; i < IDT_ENTRIES; ++i) {
     set_idt_entry(i, isr_handlers[i], isr_ist[i], IDT_INTERRUPT_GATE_FLAGS);
   }
+}
 
-  idtp.limit = sizeof(IDTEntry) * 256 - 1; // ainda reserva o total (256)
+void init_idt() {
+  Logf(LogLevel::INFO, "Starting IDT initialization...");
+
+  init_idt_entries();
+  init_exception_handlers();
+
+  idtp.limit = sizeof(IDTEntry) * IDT_ENTRIES - 1;
   idtp.base = reinterpret_cast<uint64_t>(&idt);
 
   Logf(LogLevel::INFO,
-       "IDT pointer constructed at base 0x%lx, limit %u. Executing lidt...",
+       "IDT pointer set at base 0x%lx, limit %u. Loading IDT with lidt...",
        idtp.base, idtp.limit);
+
   flush_idt(&idtp);
-  Logf(LogLevel::INFO, "IDT successfully loaded.");
+
+  Logf(LogLevel::INFO, "IDT loaded successfully.");
 }
