472231fc324dca853f3fcc208aceab442caee9ca
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-20 15:45:06 -0300

    feat(early init): the first thing we can need do its calling gdt

---
+++ Src/Kernel/Boot/early_init.cpp
@@ -0,0 +1,11 @@
+#include <Kernel/Arch/x86_64/gdt.h>
+#include <Kernel/Boot/early_init.h>
+#include <Kernel/Boot/multiboot2.h>
+
+#include <LibFK/Log.hpp>
+
+void early_init(multiboot2::TagMemoryMap)
+{
+    Log(LogLevel::INFO, "Init the kernel");
+    gdt::Manager::instance().initialize();
+}
--- Src/Kernel/Boot/kmain.cpp
+++ Src/Kernel/Boot/kmain.cpp
@@ -1,5 +1,8 @@
+#include <Kernel/Boot/early_init.h>
 #include <Kernel/Boot/kmain.h>
 #include <Kernel/Driver/VgaBuffer.h>
+
+#include <LibFK/Log.hpp>
 
 void kmain(LibC::uint32_t multiboot_magic, void* mb_info)
 {
@@ -11,27 +14,26 @@ void kmain(LibC::uint32_t multiboot_magic, void* mb_info)
         }
     }
 
-    auto const* info = reinterpret_cast<Info const*>(mb_info);
+    auto const info = reinterpret_cast<Info const*>(mb_info);
     MultibootParser mb_parser(info);
 
-    auto const* mem_map_tag = mb_parser.find_tag<TagMemoryMap>(TagType::MMap);
+    auto* mem_map_tag = mb_parser.find_tag<TagMemoryMap>(TagType::MMap);
     if (!mem_map_tag) {
         while (true) {
             __asm__("hlt");
         }
     }
 
-    LibC::uint64_t total_mem = 0;
-    for (auto const* entry = mem_map_tag->begin(); entry != mem_map_tag->end();
-         ++entry) {
-        if (entry->type == 1) {
-            total_mem += entry->length;
-        }
-    }
-
     console.set_color(vga::Color::LightGreen, vga::Color::Black);
     console.clear();
-    console.write("Hello World!");
+
+#ifdef FKERNEL_DEBUG
+    Logger::Instance().SetLevel(LogLevel::TRACE);
+#else
+    Logger::Instance().SetLevel(LogLevel::INFO);
+#endif
+
+    early_init(*mem_map_tag);
 
     // NOTE: This infinite loop is the end point of kernel, our goal is never reach here
     while (true) {
