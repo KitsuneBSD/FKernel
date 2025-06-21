f32a122d4c4f688421b6e5ba5b31007a5a8b60fe
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-21 02:44:47 -0300

    feat(early-init): instance the idt and set the exceptions

--- Src/Kernel/Boot/early_init.cpp
+++ Src/Kernel/Boot/early_init.cpp
@@ -1,4 +1,5 @@
 #include <Kernel/Arch/x86_64/gdt.h>
+#include <Kernel/Arch/x86_64/idt.h>
 #include <Kernel/Boot/early_init.h>
 #include <Kernel/Boot/multiboot2.h>
 
@@ -8,4 +9,7 @@ void early_init(multiboot2::TagMemoryMap)
 {
     Log(LogLevel::INFO, "Init the kernel");
     gdt::Manager::instance().initialize();
+    idt::Manager::instance().initialize();
+
+    asm("ud2");
 }
