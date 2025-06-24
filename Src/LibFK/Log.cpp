3e870b39af10f339c2ac409ccec8065f4ebd68a8
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-24 14:24:04 -0300

    feat(log): combine SerialPort and String Implementation to make a log

---
+++ Src/LibFK/Log.cpp
@@ -0,0 +1,58 @@
+#include <Kernel/Driver/SerialPort.h>
+#include <LibFK/Log.h>
+
+#include <LibC/stdarg.h>
+#include <LibC/stdio.h>
+
+LogLevel Logger::currentLevel = LogLevel::INFO;
+
+Logger& Logger::Instance()
+{
+    static Logger instance;
+    return instance;
+}
+
+char const* Logger::LevelToString(LogLevel level)
+{
+    switch (level) {
+    case LogLevel::TRACE:
+        return "TRACE";
+    case LogLevel::INFO:
+        return "INFO";
+    case LogLevel::WARN:
+        return "WARN";
+    case LogLevel::ERROR:
+        return "ERROR";
+    default:
+        return "UNKNOWN";
+    }
+}
+
+void Logger::SetLevel(LogLevel level) { currentLevel = level; }
+
+void Logger::Log(LogLevel level, char const* message) const
+{
+    if (level < currentLevel)
+        return;
+
+    auto& serial = Serial::SerialPort::Instance();
+
+    serial.write("[");
+    serial.write(LevelToString(level));
+    serial.write("] ");
+    serial.write(message);
+    serial.write("\n");
+}
+void Logger::Logf(LogLevel level, char const* fmt, ...) const
+{
+    if (level < currentLevel)
+        return;
+
+    char buffer[512];
+    va_list args;
+    va_start(args, fmt);
+    LibC::vsprintf(buffer, fmt, args);
+    va_end(args);
+
+    Log(level, buffer);
+}
