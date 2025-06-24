26739210cf05c9d65c6eca769b93cba82481eb41
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-24 14:24:40 -0300

    feat(kmain): calling a GDT in kmain
    (probably i'll separe in early_init, init, late_init on future)

--- Src/Kernel/Init/kmain.cpp
+++ Src/Kernel/Init/kmain.cpp
@@ -1,4 +1,25 @@
+#include "LibFK/Log.h"
+#include <Kernel/Arch/x86_64/Segments/Gdt.h>
+
 #include <Kernel/Boot/multiboot2.h>
 #include <Kernel/Boot/multiboot_interpreter.h>
 
-extern "C" void kmain(const multiboot2::Info &multiboot_ptr) { return; }
+#include <Kernel/Driver/SerialPort.h>
+
+extern "C" void kmain(multiboot2::Info const& multiboot_ptr)
+{
+
+#ifdef FKERNEL_DEBUG
+    Logger::Instance().SetLevel(LogLevel::TRACE);
+#else
+    Logger::Instance().SetLevel(LogLevel::INFO);
+#endif
+
+    auto& manager
+        = gdt::Manager::Instance();
+    manager.initialize();
+
+    Log(LogLevel::INFO, "Kernel Started");
+
+    return;
+}
