7663261d00b0d1429d1fdc7109df67818c807e03
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-24 17:43:50 -0300

    feat(kmain): get the memory_map, check the kernel status before jump for early_init

--- Src/Kernel/Init/kmain.cpp
+++ Src/Kernel/Init/kmain.cpp
@@ -1,25 +1,39 @@
-#include "LibFK/Log.h"
-#include <Kernel/Arch/x86_64/Segments/Gdt.h>
+#include "LibC/stdint.h"
+#include <LibFK/Log.h>
 
 #include <Kernel/Boot/multiboot2.h>
 #include <Kernel/Boot/multiboot_interpreter.h>
 
+#include <Kernel/Boot/early_init.h>
+
 #include <Kernel/Driver/SerialPort.h>
 
-extern "C" void kmain(multiboot2::Info const& multiboot_ptr)
+extern "C" void kmain(LibC::uint32_t multiboot2_magic, void* multiboot_ptr)
 {
+    if (multiboot2_magic != multiboot2::BOOTLOADER_MAGIC) {
+        Logf(LogLevel::ERROR, "Received magic %d is different than %d", multiboot2_magic, multiboot2::BOOTLOADER_MAGIC);
+        while (true) {
+            __asm__("hlt");
+        }
+    }
 
+    multiboot2::MultibootParser mb_parser(multiboot_ptr);
 #ifdef FKERNEL_DEBUG
     Logger::Instance().SetLevel(LogLevel::TRACE);
 #else
     Logger::Instance().SetLevel(LogLevel::INFO);
 #endif
 
-    auto& manager
-        = gdt::Manager::Instance();
-    manager.initialize();
+    auto mem_map_tag = mb_parser.find_tag<multiboot2::TagMemoryMap>(multiboot2::TagType::MMap);
 
-    Log(LogLevel::INFO, "Kernel Started");
+    if (!mem_map_tag) {
+        Log(LogLevel::ERROR, "Memory Map not found");
+        while (true) {
+
+            __asm__("hlt");
+        }
+    }
+    // early_init(*mem_map_tag);
 
     return;
 }
