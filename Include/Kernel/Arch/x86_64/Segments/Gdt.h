2ad50087404dcb7ba80bccc10f1e51cb2a191e80
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-25 09:04:45 -0300

    feat(gdt/tss): implement TSS in a GDT

--- Include/Kernel/Arch/x86_64/Segments/Gdt.h
+++ Include/Kernel/Arch/x86_64/Segments/Gdt.h
@@ -4,6 +4,8 @@
 #include <LibC/stdint.h>
 
 #include <Kernel/Arch/x86_64/Cpu/Asm.h>
+#include <Kernel/Arch/x86_64/Cpu/Constants.h>
+#include <Kernel/Arch/x86_64/Segments/Tss.h>
 
 namespace gdt {
 enum GdtIndex : LibC::uint8_t {
@@ -36,8 +38,9 @@ static_assert(sizeof(gdt::gdt_pointer) == 10, "gdt_pointer must be 10 bytes");
 // Manager is a singleton class (in this moment) to set the global descriptor table
 class Manager {
 private:
-    gdt_entry entries_[5];
+    gdt_entry entries_[IST_COUNT];
     gdt_pointer gdtr;
+    Tss::Tss tss_;
 
     Manager() = default;
 
@@ -53,6 +56,7 @@ public:
 
     void initialize() noexcept;
     void set_entry(int index, LibC::uint32_t base, LibC::uint32_t limit, LibC::uint8_t access, LibC::uint8_t granularity) noexcept;
+    void set_tss_entry(int index, LibC::uint64_t base, LibC::uint32_t limit) noexcept;
 };
 
 }
