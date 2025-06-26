09c7c3e8ac3ec3485a6b0dc01460cdb204d85bcd
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-26 13:30:58 -0300

    feat(early init): calling the physical memory manager when we start the kernel

--- Src/Kernel/Init/early_init.cpp
+++ Src/Kernel/Init/early_init.cpp
@@ -1,8 +1,7 @@
-#include "LibC/stdint.h"
-#include "LibFK/Log.h"
 #include <Kernel/Arch/x86_64/Segments/Gdt.h>
 #include <Kernel/Arch/x86_64/Segments/Idt.h>
 #include <Kernel/Boot/early_init.h>
+#include <Kernel/MemoryManagement/PhysicalMemoryManagement/PhysicalMemoryManager.h>
 
 extern "C" LibC::uintptr_t current_pml4_ptr;
 
@@ -15,4 +14,6 @@ void early_init(multiboot2::TagMemoryMap const& mmap)
 
     auto& idt_manager = idt::Manager::Instance();
     idt_manager.initialize();
+
+    MemoryManagement::PhysicalMemoryManager::initialize(mmap);
 }
