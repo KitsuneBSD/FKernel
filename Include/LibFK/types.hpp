c6d3e71ddcfd51c39e8530dc816e22959c3afd94
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-29 11:58:05 -0300

    feat(types): add a macro when a flag is unused

--- Include/LibFK/types.hpp
+++ Include/LibFK/types.hpp
@@ -17,4 +17,10 @@ typedef LibC::int8_t signed_qword;
 constexpr unsigned KiB = 1024;
 constexpr unsigned MiB = KiB * KiB;
 constexpr unsigned GiB = MiB * KiB;
+
 }
+
+#define UNUSED(x) \
+    do {          \
+        (void)x;  \
+    } while (0);
