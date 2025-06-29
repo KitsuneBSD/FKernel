1929b5a4118fb211e3460f67e14f0829498df2bf
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-06-29 11:59:20 -0300

    refactor(routines): use the flag unused in all routines

--- Src/Kernel/Arch/x86_64/Interrupts/routines.cpp
+++ Src/Kernel/Arch/x86_64/Interrupts/routines.cpp
@@ -4,8 +4,9 @@
 #include <Kernel/Arch/x86_64/Segments/Idt.h>
 #include <LibC/stdint.h>
 #include <LibFK/Log.h>
+#include <LibFK/types.hpp>
 
-static LibC::uint64_t volatile tick_count = 0;
+static LibC::uint64_t tick_count = 0;
 
 idt::IrqHandler irq_handlers[16] = { nullptr };
 
@@ -42,6 +43,9 @@ extern "C" void (*const routine_stubs[16])() = {
 
 extern "C" void timer_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     ++tick_count;
 
     Io::send_eoi(0);
@@ -49,6 +53,9 @@ extern "C" void timer_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void keyboard_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     LibC::uint8_t scancode = Io::inb(0x60);
 
     Logf(LogLevel::TRACE, "Keyboard IRQ - scancode: 0x%02X", scancode);
@@ -58,6 +65,8 @@ extern "C" void keyboard_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void cascade_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
 
     Logf(LogLevel::TRACE, "Cascade IRQ2 triggered.");
 
@@ -66,6 +75,9 @@ extern "C" void cascade_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void com2_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     Logf(LogLevel::TRACE, "Serial COM2 IRQ3 triggered.");
 
     Io::send_eoi(3);
@@ -73,6 +85,9 @@ extern "C" void com2_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void com1_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     Logf(LogLevel::TRACE, "Serial COM1 IRQ4 triggered.");
 
     Io::send_eoi(4);
@@ -80,6 +95,9 @@ extern "C" void com1_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void legacy_peripheral_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     Logf(LogLevel::TRACE, "Legacy peripheral IRQ5 triggered.");
 
     Io::send_eoi(5);
@@ -87,6 +105,9 @@ extern "C" void legacy_peripheral_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void fdc_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     Logf(LogLevel::TRACE, "Floppy Disk Controller IRQ6 triggered.");
 
     Io::send_eoi(6);
@@ -94,6 +115,9 @@ extern "C" void fdc_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void spurious_irq7_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     Logf(LogLevel::WARN, "IRQ7 triggered — possibly spurious.");
 
     Io::send_eoi(7);
@@ -101,6 +125,9 @@ extern "C" void spurious_irq7_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void rtc_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     Logf(LogLevel::TRACE, "Real-Time Clock IRQ8 triggered.");
 
     Io::send_eoi(8);
@@ -108,6 +135,9 @@ extern "C" void rtc_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void acpi_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     Logf(LogLevel::TRACE, "ACPI / IRQ2 redirected IRQ9 triggered.");
 
     Io::send_eoi(9);
@@ -115,6 +145,9 @@ extern "C" void acpi_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void irq10_pci_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     Logf(LogLevel::TRACE, "IRQ10 triggered (PCI / livre).");
 
     Io::send_eoi(10);
@@ -122,6 +155,9 @@ extern "C" void irq10_pci_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void irq11_pci_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     Logf(LogLevel::TRACE, "IRQ11 triggered (PCI / livre).");
 
     Io::send_eoi(11);
@@ -129,6 +165,9 @@ extern "C" void irq11_pci_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void ps2_mouse_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     Logf(LogLevel::TRACE, "PS/2 Mouse IRQ12 triggered.");
 
     Io::send_eoi(12);
@@ -136,6 +175,9 @@ extern "C" void ps2_mouse_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void fpu_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     Logf(LogLevel::TRACE, "FPU / x87 Coprocessor IRQ13 triggered.");
 
     Io::send_eoi(13);
@@ -143,6 +185,9 @@ extern "C" void fpu_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void primary_ata_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     Logf(LogLevel::TRACE, "Primary ATA IRQ14 triggered.");
 
     Io::send_eoi(14);
@@ -150,6 +195,9 @@ extern "C" void primary_ata_handler(LibC::uint8_t irq, void* context)
 
 extern "C" void secondary_ata_handler(LibC::uint8_t irq, void* context)
 {
+    UNUSED(irq)
+    UNUSED(context)
+
     Logf(LogLevel::TRACE, "Secondary ATA IRQ15 triggered.");
     Io::send_eoi(15);
 }
