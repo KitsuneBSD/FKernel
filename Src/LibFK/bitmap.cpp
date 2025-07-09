56bd2fc66a9a2b36ac3effbd757c832d91c71b7d
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-07-09 11:52:12 -0300

    feat(libfk): update IntrusiveList and Bitmap

--- Src/LibFK/bitmap.cpp
+++ Src/LibFK/bitmap.cpp
@@ -2,22 +2,30 @@
 #include <LibFK/log.h>
 
 namespace FK {
+
 bool Bitmap::test(LibC::size_t bit) noexcept
 {
     LibC::size_t idx = bit / 64;
     LibC::size_t offset = bit % 64;
-    if (idx >= size_ / 64)
+    if (idx >= size_ / 64) {
+        Logf(LogLevel::TRACE, "Bitmap: test out of range: bit=%lu (size=%lu)", bit, size_);
         return false;
-    return (data_[idx] & (1ULL << offset)) != 0;
+    }
+    bool result = (data_[idx] & (1ULL << offset)) != 0;
+    Logf(LogLevel::TRACE, "Bitmap: test bit=%lu -> %d", bit, result);
+    return result;
 }
 
 bool Bitmap::set(LibC::size_t bit) noexcept
 {
     LibC::size_t idx = bit / 64;
     LibC::size_t offset = bit % 64;
-    if (idx >= size_ / 64)
+    if (idx >= size_ / 64) {
+        Logf(LogLevel::TRACE, "Bitmap: set out of range: bit=%lu (size=%lu)", bit, size_);
         return false;
+    }
     data_[idx] |= (1ULL << offset);
+    Logf(LogLevel::TRACE, "Bitmap: set bit=%lu", bit);
     return true;
 }
 
@@ -25,23 +33,44 @@ bool Bitmap::clear(LibC::size_t bit) noexcept
 {
     LibC::size_t idx = bit / 64;
     LibC::size_t offset = bit % 64;
-    if (idx >= size_ / 64)
+    if (idx >= size_ / 64) {
+        Logf(LogLevel::TRACE, "Bitmap: clear out of range: bit=%lu (size=%lu)", bit, size_);
         return false;
+    }
     data_[idx] &= ~(1ULL << offset);
+    Logf(LogLevel::TRACE, "Bitmap: clear bit=%lu", bit);
     return true;
 }
 
 bool Bitmap::find_first_clear(LibC::size_t& out_bit, LibC::size_t start) const noexcept
 {
+    Logf(LogLevel::TRACE, "Bitmap: find_first_clear starting at %lu", start);
     for (LibC::size_t i = start / 64; i < size_ / 64; ++i) {
         if (data_[i] != LibC::UINT64_MAX) {
             LibC::uint64_t inverted = ~data_[i];
             LibC::size_t bit_pos = __builtin_ctzll(inverted);
             out_bit = i * 64 + bit_pos;
-            if (out_bit < size_)
+            if (out_bit < size_) {
+                Logf(LogLevel::TRACE, "Bitmap: find_first_clear found bit=%lu", out_bit);
                 return true;
+            }
         }
     }
+    Log(LogLevel::TRACE, "Bitmap: find_first_clear no free bit found");
     return false;
+}
+
+void Bitmap::reset(LibC::uint64_t* buffer, LibC::size_t bits) noexcept
+{
+    Logf(LogLevel::TRACE, "Bitmap: reset buffer=%p bits=%lu", buffer, bits);
+    if (!buffer || bits == 0) {
+        data_ = nullptr;
+        size_ = 0;
+        Log(LogLevel::TRACE, "Bitmap: reset: invalid buffer or size, clearing state");
+        return;
+    }
+
+    data_ = buffer;
+    size_ = bits;
 }
 }
