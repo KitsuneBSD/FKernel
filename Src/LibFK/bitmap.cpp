18717853ac25f32f97b956f7a7d840712563427a
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-07-11 08:48:07 -0300

    fix(bitmap index calc): now we calc the index based in word count

--- Src/LibFK/bitmap.cpp
+++ Src/LibFK/bitmap.cpp
@@ -1,3 +1,4 @@
+#include <LibC/string.h>
 #include <LibFK/bitmap.h>
 #include <LibFK/log.h>
 
@@ -7,7 +8,8 @@ bool Bitmap::test(LibC::size_t bit) noexcept
 {
     LibC::size_t idx = bit / 64;
     LibC::size_t offset = bit % 64;
-    if (idx >= size_ / 64) {
+    LibC::size_t word_count = (size_ + 63) / 64;
+    if (idx >= word_count) {
         Logf(LogLevel::WARN, "Bitmap: test out of range: bit=%lu (size=%lu)", bit, size_);
         return false;
     }
@@ -19,7 +21,8 @@ bool Bitmap::set(LibC::size_t bit) noexcept
 {
     LibC::size_t idx = bit / 64;
     LibC::size_t offset = bit % 64;
-    if (idx >= size_ / 64) {
+    LibC::size_t word_count = (size_ + 63) / 64;
+    if (idx >= word_count) {
         Logf(LogLevel::WARN, "Bitmap: set out of range: bit=%lu (size=%lu)", bit, size_);
         return false;
     }
@@ -31,7 +34,8 @@ bool Bitmap::clear(LibC::size_t bit) noexcept
 {
     LibC::size_t idx = bit / 64;
     LibC::size_t offset = bit % 64;
-    if (idx >= size_ / 64) {
+    LibC::size_t word_count = (size_ + 63) / 64;
+    if (idx >= word_count) {
         Logf(LogLevel::WARN, "Bitmap: clear out of range: bit=%lu (size=%lu)", bit, size_);
         return false;
     }
@@ -41,7 +45,8 @@ bool Bitmap::clear(LibC::size_t bit) noexcept
 
 bool Bitmap::find_first_clear(LibC::size_t& out_bit, LibC::size_t start) const noexcept
 {
-    for (LibC::size_t i = start / 64; i < size_ / 64; ++i) {
+    LibC::size_t word_count = (size_ + 63) / 64;
+    for (LibC::size_t i = start / 64; i < word_count; ++i) {
         if (data_[i] != LibC::UINT64_MAX) {
             LibC::uint64_t inverted = ~data_[i];
             LibC::size_t bit_pos = __builtin_ctzll(inverted);
@@ -67,5 +72,10 @@ void Bitmap::reset(LibC::uint64_t* buffer, LibC::size_t bits) noexcept
 
     data_ = buffer;
     size_ = bits;
+
+    LibC::size_t word_count = (bits + 63) / 64;
+
+    // Zeroiza todo o buffer usando memset
+    LibC::memset(data_, 0, word_count * sizeof(LibC::uint64_t));
 }
 }
