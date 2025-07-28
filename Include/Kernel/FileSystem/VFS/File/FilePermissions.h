3b496f5bf0a13ad1d2370844a1e3462745876124
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-07-28 18:23:19 -0300

    chore(todo): add a todo comentary to fix later

--- Include/Kernel/FileSystem/VFS/File/FilePermissions.h
+++ Include/Kernel/FileSystem/VFS/File/FilePermissions.h
@@ -4,6 +4,7 @@
 
 namespace FileSystem {
 
+// TODO: Turn this code as design pattern builder
 constexpr LibC::uint32_t O_WRONLY = 1 << 0;
 constexpr LibC::uint32_t O_RDWR = 1 << 1;
 constexpr LibC::uint32_t O_TRUNC = 1 << 2;
