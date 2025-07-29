8c8b623931d8dcd5e097a9f476e28f148c54a868
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-07-29 15:15:17 -0300

    feat(string.h): add function to copy string

--- Include/LibC/string.h
+++ Include/LibC/string.h
@@ -15,13 +15,12 @@ int atoi(char const* str);
 char* itoa(int num, char* str, int base);
 
 extern "C" int strcmp(char const* a, char const* b);
-
 extern "C" int strncmp(char const* s1, char const* s2, LibC::size_t n);
-
 extern "C" char* strncpy(char* dest, char const* src, LibC::size_t n);
 
 extern "C" LibC::size_t strlen(char const* str);
 
 extern "C" LibC::size_t strlcpy(char* dst, char const* src, LibC::size_t size);
+extern "C" char* strcpy(char* dest, char const* src);
 
 }
