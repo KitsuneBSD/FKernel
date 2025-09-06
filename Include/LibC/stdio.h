567829d92334b429cb90e396591b473f93751973
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-06 17:14:27 -0300

    feat(stdio): apply function to print and format strings

--- Include/LibC/stdio.h
+++ Include/LibC/stdio.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "Kernel/Driver/Vga/Vga_buffer.h"
 #include <Kernel/Driver/SerialPort/Serial.h>
+#include <Kernel/Driver/Vga/Vga_buffer.h>
 
 #include <LibC/stdarg.h>
 #include <LibC/stddef.h>
@@ -11,51 +11,8 @@
 extern "C" {
 #endif
 
-void kprintf(const char *fmt, ...) {
-
-  va_list args;
-  va_start(args, fmt);
-
-  for (size_t i = 0; fmt[i]; ++i) {
-    if (fmt[i] != '%') {
-      Serial::write_char(fmt[i]);
-      VGA::instance().put_char(fmt[i]);
-      continue;
-    }
-
-    ++i; // pula o '%'
-    char spec = fmt[i];
-    switch (spec) {
-    case 's': {
-      const char *s = va_arg(args, const char *);
-      Serial::write(s);
-      VGA::instance().write(s);
-      break;
-    }
-    case 'd': {
-      int val = va_arg(args, int);
-      Serial::write_dec(val);
-      break;
-    }
-    case 'x': {
-      unsigned int val = va_arg(args, unsigned int);
-      Serial::write_hex(val);
-      break;
-    }
-    case '%': {
-      Serial::write_char('%');
-      break;
-    }
-    default: {
-      Serial::write_char('%');
-      Serial::write_char(spec);
-      break;
-    }
-    }
-  }
-
-  va_end(args);
-}
+int snprintf(char *str, size_t size, const char *fmt, ...);
+void kprintf(const char *fmt, ...);
 
 #ifdef __cplusplus
 }
