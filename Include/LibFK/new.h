722b04aca3499eaf89a5246b616641c62b49d377
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-14 12:00:07 -0300

    fix(new): add a placement new

--- Include/LibFK/new.h
+++ Include/LibFK/new.h
@@ -2,6 +2,10 @@
 
 #include <LibFK/heap_malloc.h>
 
+void *operator new(size_t, void *ptr) noexcept { return ptr; }
+
+void *operator new[](size_t, void *ptr) noexcept { return ptr; }
+
 void *operator new(size_t size) { return kmalloc(size); }
 
 void *operator new[](size_t size) { return kmalloc(size); }
@@ -13,3 +17,7 @@ void operator delete[](void *ptr) { return kfree(ptr); }
 void operator delete(void *ptr, size_t) { return kfree(ptr); }
 
 void operator delete[](void *ptr, size_t) { return kfree(ptr); }
+
+void operator delete(void *, void *) noexcept {}
+
+void operator delete[](void *, void *) noexcept {}
