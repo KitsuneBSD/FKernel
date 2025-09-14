614e23fc8ccb0586b0bc1e77c8a7d89f228ebcfd
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-14 12:01:01 -0300

    fix(optional): can destroyed without use memset

--- Include/LibFK/optional.h
+++ Include/LibFK/optional.h
@@ -2,23 +2,24 @@
 
 #include <LibC/stddef.h>
 #include <LibC/string.h>
+#include <LibFK/new.h>
 
 template <typename T> class optional {
 private:
-  bool has_value_;
+  bool has_value_{false};
   alignas(T) unsigned char storage[sizeof(T)];
 
-public:
-  constexpr optional() : has_value_(false) {}
+  T *ptr() { return reinterpret_cast<T *>(storage); }
+  const T *ptr() const { return reinterpret_cast<const T *>(storage); }
 
-  optional(const T &value) : has_value_(true) {
-    memcpy(storage, &value, sizeof(T));
-  }
+public:
+  constexpr optional() = default;
+
+  optional(const T &value) : has_value_(true) { new (storage) T(value); }
 
   optional(const optional &other) : has_value_(other.has_value_) {
-    if (has_value_) {
-      memcpy(storage, other.storage, sizeof(T));
-    }
+    if (has_value_)
+      new (storage) T(*other.ptr());
   }
 
   ~optional() { reset(); }
@@ -27,7 +28,7 @@ public:
     if (this != &other) {
       reset();
       if (other.has_value_) {
-        memcpy(storage, other.storage, sizeof(T));
+        new (storage) T(*other.ptr());
         has_value_ = true;
       }
     }
@@ -36,13 +37,12 @@ public:
 
   constexpr bool has_value() const { return has_value_; }
 
-  T &value() { return *reinterpret_cast<T *>(storage); }
-
-  const T &value() const { return *reinterpret_cast<const T *>(storage); }
+  T &value() { return *ptr(); }
+  const T &value() const { return *ptr(); }
 
   void reset() {
     if (has_value_) {
-      memset(storage, 0, sizeof(T));
+      ptr()->~T();
       has_value_ = false;
     }
   }
