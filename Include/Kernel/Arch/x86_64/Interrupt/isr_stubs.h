9b00af31acdffd453d19cc5cd8d9de3aae85d661
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-18 15:52:08 -0300

    feat(interrupts): print exception name when this is unknowledge

--- Include/Kernel/Arch/x86_64/Interrupt/isr_stubs.h
+++ Include/Kernel/Arch/x86_64/Interrupt/isr_stubs.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <Kernel/Arch/x86_64/Interrupt/idt_types.h>
 extern "C" {
 
 void isr0();
@@ -53,11 +54,11 @@ void irq14();
 void irq15();
 }
 
-static void (*g_isr_stubs[32])() = {
+static void (*g_isr_stubs[MAX_X86_64_ISR_SIZE])() = {
     isr0,  isr1,  isr2,  isr3,  isr4,  isr5,  isr6,  isr7,  isr8,  isr9,  isr10,
     isr11, isr12, isr13, isr14, isr15, isr16, isr17, isr18, isr19, isr20, isr21,
     isr22, isr23, isr24, isr25, isr26, isr27, isr28, isr29, isr30, isr31};
 
-static void (*g_irq_stubs[16])() = {irq0,  irq1,  irq2,  irq3, irq4,  irq5,
-                                    irq6,  irq7,  irq8,  irq9, irq10, irq11,
-                                    irq12, irq13, irq14, irq15};
+static void (*g_irq_stubs[MAX_X86_64_IRQ_SIZE])() = {
+    irq0, irq1, irq2,  irq3,  irq4,  irq5,  irq6,  irq7,
+    irq8, irq9, irq10, irq11, irq12, irq13, irq14, irq15};
