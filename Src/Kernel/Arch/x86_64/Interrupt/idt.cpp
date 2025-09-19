a083828555ca0f666ffb8d42e3e7aae78236a136
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-19 10:15:47 -0300

    feat(interrupts): add a klog function to show what open

--- Src/Kernel/Arch/x86_64/Interrupt/idt.cpp
+++ Src/Kernel/Arch/x86_64/Interrupt/idt.cpp
@@ -1,15 +1,20 @@
-#include "Kernel/Arch/x86_64/Interrupt/idt_types.h"
 #include <Kernel/Arch/x86_64/Interrupt/idt.h>
+#include <Kernel/Arch/x86_64/Interrupt/idt_types.h>
 #include <Kernel/Arch/x86_64/Interrupt/isr_stubs.h>
-#include <LibC/stdio.h>
+#include <LibFK/log.h>
 
-idt::idt() { clear(); }
+idt::idt() {
+  clear();
+  klog("IDT", "IDT initialized.");
+}
 
 void idt::clear() {
   for (size_t i = 0; i < MAX_X86_64_IDT_SIZE; ++i) {
     entries[i] = {};
     isr_handlers[i] = nullptr;
   }
+
+  klog("IDT", "All Idt entries are cleaned");
 }
 
 void idt::set_gate(uint8_t vector, void (*handler_ptr)(), uint16_t selector,
@@ -23,10 +28,15 @@ void idt::set_gate(uint8_t vector, void (*handler_ptr)(), uint16_t selector,
   d.offset_mid = static_cast<uint16_t>((handler >> 16) & 0xFFFFu);
   d.offset_high = static_cast<uint32_t>((handler >> 32) & 0xFFFFFFFFu);
   d.zero = 0;
+
+  klog("IDT", "Gate set for vector %u, handler %p", vector, handler_ptr);
 }
 
 void idt::register_isr_handler(uint8_t vector, isr_handler_t handler) {
   isr_handlers[vector] = handler;
+
+  klog("IDT", "ISR handler registered for vector %u, handler %p", vector,
+       handler);
 }
 isr_handler_t idt::get_isr_handler(uint8_t vec) const {
   return isr_handlers[vec];
@@ -34,6 +44,8 @@ isr_handler_t idt::get_isr_handler(uint8_t vec) const {
 
 void idt::register_irq_handler(uint8_t vector, irq_handler_t handler) {
   irq_handlers[vector] = handler;
+  klog("IDT", "IRQ handler registered for vector %u, handler %p", vector,
+       handler);
 }
 
 irq_handler_t idt::get_irq_handler(uint8_t vec) const {
@@ -45,6 +57,8 @@ void idt::load() {
   ptr.limit = static_cast<uint16_t>(sizeof(entries) - 1);
   ptr.base = reinterpret_cast<uint64_t>(&entries);
   flush_idt(&ptr);
+
+  klog("IDT", "IDT loaded successfully.");
 }
 
 Idt_Entry *idt::raw_entries() { return entries.begin(); }
