3bfc0892db338f3128b1199ba01e0bc294318305
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-19 10:41:55 -0300

    feat(early_init): add a message on release mode to show IDT loaded with sucess

--- Src/Kernel/Arch/x86_64/Init/early_init.cpp
+++ Src/Kernel/Arch/x86_64/Init/early_init.cpp
@@ -2,9 +2,10 @@
 #include <Kernel/Boot/early_init.h>
 #include <LibC/stddef.h>
 #include <LibC/stdio.h>
+#include <LibFK/log.h>
 #include <LibFK/type_traits.h>
 
-void early_init(const multiboot2::TagMemoryMap &mmap) {
-  kprintf("Reference to multiboot2 memory map: %p\n", mmap);
+void early_init([[maybe_unused]] const multiboot2::TagMemoryMap &mmap) {
+  klog("MULTIBOOT2", "Reference to multiboot2 memory map: %p\n", mmap);
   init_idt();
 }
