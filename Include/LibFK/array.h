ba15607024743ab2d06443e83b27ef54f6858023
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-20 13:00:46 -0300

    :sparkles: (array) add a method fill to array

--- Include/LibFK/array.h
+++ Include/LibFK/array.h
@@ -4,15 +4,21 @@
 #include <LibFK/type_traits.h>
 
 template <typename T, size_t N> struct array {
-  T array[N];
+  T data_[N];
 
   constexpr size_t size() const { return N; }
-  constexpr T &operator[](size_t i) { return array[i]; }
-  constexpr const T &operator[](size_t i) const { return array[i]; }
+  constexpr T &operator[](size_t i) { return data_[i]; }
+  constexpr const T &operator[](size_t i) const { return data_[i]; }
 
-  T *begin() { return array; }
-  T *end() { return array + N; }
+  T *begin() { return data_; }
+  T *end() { return data_ + N; }
 
-  const T *begin() const { return array; }
-  const T *end() const { return array + N; }
+  constexpr void fill(const T &value) {
+    for (size_t i = 0; i < N; ++i) {
+      data_[i] = value;
+    }
+  }
+
+  const T *begin() const { return data_; }
+  const T *end() const { return data_ + N; }
 };
