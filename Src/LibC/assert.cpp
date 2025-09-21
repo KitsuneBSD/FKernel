8d568f94738e8a2f6f8b2320d497b6b94e980e0e
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-21 17:55:38 -0300

    :bug: (assert) remove __builtin_unreachable is unutil on this kernel

--- Src/LibC/assert.cpp
+++ Src/LibC/assert.cpp
@@ -1,16 +1,11 @@
 #include <LibC/assert.h>
-#include <LibFK/log.h>
+#include <LibC/stdio.h>
 
 extern "C" void __kernel_assert_fail(const char *expr, const char *file,
                                      int line, const char *func) {
-  klog("ASSERT", KLOG_COLOR_RED, "Failed: %s (%s: %s: %d)\n", expr, file, func,
-       line);
+  kprintf("Assert Failed: %s (%s: %s: %d)\n", expr, file, func, line);
 
-#ifdef FKERNEL_DEBUG
   while (1) {
     __asm__ volatile("cli; hlt");
   }
-#endif
-
-  __builtin_unreachable();
 }
