c69182478f144f567b8941339255d690dea20128
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-25 12:58:30 -0300

    feat(early_init): remove multiboot2_memorymap parsing from early_init

--- Src/Kernel/Arch/x86_64/Init/early_init.cpp
+++ Src/Kernel/Arch/x86_64/Init/early_init.cpp
@@ -6,14 +6,8 @@
 
 #include <Kernel/Arch/x86_64/Interrupt/interrupt_controller.h>
 
-void early_init([[maybe_unused]] const multiboot2::TagMemoryMap* mmap) {
+void early_init([[maybe_unused]] const multiboot2::TagMemoryMap *mmap) {
   klog("MULTIBOOT2", "Reference to multiboot2 memory map: %p", mmap);
-
-  for (auto entry : *mmap) {
-    if (entry.type == 1) {
-      kprintf("Memory map entry: addr=%p, len=%u, type=%u\n", entry.base_addr, entry.length, entry.type);  
-    }
-  } 
 
   InterruptController::the().initialize();
 }
