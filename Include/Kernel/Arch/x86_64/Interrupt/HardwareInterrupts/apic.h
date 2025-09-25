a6be8a4904b3c7a5d87ec9bd0e20f82e00214611
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-25 15:00:52 -0300

    feat(apic): turn the struct local_apic bigger

--- Include/Kernel/Arch/x86_64/Interrupt/HardwareInterrupts/apic.h
+++ Include/Kernel/Arch/x86_64/Interrupt/HardwareInterrupts/apic.h
@@ -12,11 +12,28 @@ constexpr uint32_t APIC_SVR_ENABLE = 0x100; // Bit 8
 struct local_apic {
   volatile uint32_t id;
   volatile uint32_t version;
-  uint32_t reserved0[4];
   volatile uint32_t tpr;
   volatile uint32_t apr;
   volatile uint32_t ppr;
   volatile uint32_t eoi;
+  volatile uint32_t ldr;
+  volatile uint32_t dfr;
+  volatile uint32_t spurious;
+  volatile uint32_t isr[8];
+  volatile uint32_t tmr[8];
+  volatile uint32_t irr[8];
+  volatile uint32_t esr;
+  volatile uint32_t icr_low;
+  volatile uint32_t icr_high;
+  volatile uint32_t lvt_timer;
+  volatile uint32_t lvt_thermal;
+  volatile uint32_t lvt_perf;
+  volatile uint32_t lvt_lint0;
+  volatile uint32_t lvt_lint1;
+  volatile uint32_t lvt_error;
+  volatile uint32_t initial_count;
+  volatile uint32_t current_count;
+  volatile uint32_t divide_config;
 };
 
 class APIC {
