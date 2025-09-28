ccfccd23aed1ed8db5aafd8dd1771143472949d9
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-27 21:23:08 -0300

    feat(arch_defs): define the max number of chunks on x86_64

--- Include/Kernel/Arch/x86_64/arch_defs.h
+++ Include/Kernel/Arch/x86_64/arch_defs.h
@@ -2,6 +2,8 @@
 
 #include <LibC/stddef.h>
 #include <LibC/stdint.h>
+#include <LibC/limits.h>
 
 constexpr uint8_t MAX_X86_64_INTERRUPTS_LENGTH = 256;
 constexpr size_t PAGE_SIZE = 4096;
+constexpr size_t MAX_CHUNKS_PER_RANGE = UINT64_MAX;
