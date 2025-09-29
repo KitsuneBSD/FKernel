110930ef82c1c9422b62f2f85aca720737ba3880
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-29 19:10:48 -0300

    refactor(rb_tree): turn rb_tree kernel_safe

--- Include/LibFK/rb_tree.h
+++ Include/LibFK/rb_tree.h
@@ -1,175 +1,311 @@
 #pragma once
 
-#include <LibFK/log.h>
-#include <LibFK/new.h>
-
 template <typename T> class rb_node {
 private:
-  bool is_red = true;
-  T value;
-
-  rb_node<T> *parent;
-  rb_node<T> *left;
-  rb_node<T> *right;
+  bool m_red;
+  T m_value;
+  rb_node<T> *m_parent;
+  rb_node<T> *m_left;
+  rb_node<T> *m_right;
 
 public:
-  explicit rb_node(T new_value)
-      : is_red(true), value(new_value), parent(nullptr), left(nullptr),
-        right(nullptr) {}
+  rb_node()
+      : m_red(true), m_value(), m_parent(nullptr), m_left(nullptr),
+        m_right(nullptr) {}
 
-  rb_node<T>() {
-    is_red = true;
+  rb_node(const T &value)
+      : m_red(true), m_value(value), m_parent(nullptr), m_left(nullptr),
+        m_right(nullptr) {}
 
-    parent = nullptr;
-    left = nullptr;
-    right = nullptr;
+  bool is_red() const { return m_red; }
+  void set_red(bool red) { m_red = red; }
+  void toggle_color() { m_red = !m_red; }
+
+  T &value() { return m_value; }
+  const T &value() const { return m_value; }
+
+  rb_node<T> *parent() const { return m_parent; }
+  rb_node<T> *left() const { return m_left; }
+  rb_node<T> *right() const { return m_right; }
+
+  void set_parent(rb_node<T> *p) { m_parent = p; }
+
+  void set_left(rb_node<T> *l) {
+    m_left = l;
+    if (l)
+      l->m_parent = this;
   }
 
-  T get_value() { return value; }
-  bool get_color() { return is_red; }
-
-  void change_color() { is_red = !is_red; }
-
-  rb_node<T> *get_parent() { return this->parent; }
-  rb_node<T> *get_right() { return this->right; }
-  rb_node<T> *get_left() { return this->left; }
-
-  void set_color(bool color) { is_red = color; }
-  void set_parent(rb_node<T> *node) { parent = node; }
-  void set_right(rb_node<T> *node) {
-    right = node;
-    if (node)
-      node->parent = this;
-  }
-  void set_left(rb_node<T> *node) {
-    left = node;
-
-    if (node)
-      node->parent = this;
+  void set_right(rb_node<T> *r) {
+    m_right = r;
+    if (r)
+      r->m_parent = this;
   }
 };
 
-template <typename T, typename Compare = bool (*)(const T &, const T &)>
-class rb_tree {
+template <typename T, unsigned MAX_NODES = 65355> class rb_tree {
 private:
-  rb_node<T> *m_root{nullptr};
-  Compare cmp;
+  rb_node<T> *m_root;
+  rb_node<T> m_pool[MAX_NODES];
+  unsigned m_pool_index;
+
+  rb_node<T> *minimum(rb_node<T> *node) const {
+    while (node->left())
+      node = node->left();
+    return node;
+  }
+
+  void transplant(rb_node<T> *u, rb_node<T> *v) {
+    rb_node<T> *p = u->parent();
+    if (!p)
+      m_root = v;
+    else if (u == p->left())
+      p->set_left(v);
+    else
+      p->set_right(v);
+    if (v)
+      v->set_parent(p);
+  }
+
+  void fix_remove(rb_node<T> *x) {
+    while (x != m_root && (!x || !x->is_red())) {
+      rb_node<T> *p = x ? x->parent() : nullptr;
+      if (!p)
+        break;
+
+      if (x == p->left()) {
+        rb_node<T> *w = p->right();
+        if (w && w->is_red()) {
+          w->set_red(false);
+          p->set_red(true);
+          rotate_left(p);
+          w = p->right();
+        }
+        if ((!w->left() || !w->left()->is_red()) &&
+            (!w->right() || !w->right()->is_red())) {
+          w->set_red(true);
+          x = p;
+        } else {
+          if (!w->right() || !w->right()->is_red()) {
+            if (w->left())
+              w->left()->set_red(false);
+            w->set_red(true);
+            rotate_right(w);
+            w = p->right();
+          }
+          w->set_red(p->is_red());
+          p->set_red(false);
+          if (w->right())
+            w->right()->set_red(false);
+          rotate_left(p);
+          x = m_root;
+        }
+      } else {
+        rb_node<T> *w = p->left();
+        if (w && w->is_red()) {
+          w->set_red(false);
+          p->set_red(true);
+          rotate_right(p);
+          w = p->left();
+        }
+        if ((!w->left() || !w->left()->is_red()) &&
+            (!w->right() || !w->right()->is_red())) {
+          w->set_red(true);
+          x = p;
+        } else {
+          if (!w->left() || !w->left()->is_red()) {
+            if (w->right())
+              w->right()->set_red(false);
+            w->set_red(true);
+            rotate_left(w);
+            w = p->left();
+          }
+          w->set_red(p->is_red());
+          p->set_red(false);
+          if (w->left())
+            w->left()->set_red(false);
+          rotate_right(p);
+          x = m_root;
+        }
+      }
+    }
+    if (x)
+      x->set_red(false);
+  }
+  rb_node<T> *allocate_node(const T &value) {
+    if (m_pool_index >= MAX_NODES)
+      return nullptr; // pool esgotado
+
+    rb_node<T> *node = &m_pool[m_pool_index++];
+    // construtor por cópia manual
+    *node = rb_node<T>(value);
+    return node;
+  }
 
   void rotate_left(rb_node<T> *x) {
-    rb_node<T> *y = x->get_right();
-    x->set_right(y->get_left());
+    rb_node<T> *y = x->right();
+    x->set_right(y->left());
 
-    if (x->get_parent() == nullptr)
+    rb_node<T> *p = x->parent();
+    if (!p)
       m_root = y;
-    else if (x == x->get_parent()->get_left())
-      x->get_parent()->set_left(y);
+    else if (x == p->left())
+      p->set_left(y);
     else
-      x->get_parent()->set_right(y);
+      p->set_right(y);
 
     y->set_left(x);
   }
 
   void rotate_right(rb_node<T> *x) {
-    rb_node<T> *y = x->get_left();
-    x->set_left(y->get_right());
-    if (x->get_parent() == nullptr)
+    rb_node<T> *y = x->left();
+    x->set_left(y->right());
+
+    rb_node<T> *p = x->parent();
+    if (!p)
       m_root = y;
-    else if (x == x->get_parent()->get_right())
-      x->get_parent()->set_right(y);
+    else if (x == p->right())
+      p->set_right(y);
     else
-      x->get_parent()->set_left(y);
+      p->set_left(y);
 
     y->set_right(x);
   }
 
   void rebalance(rb_node<T> *node) {
-    while (node->get_parent() && node->get_parent()->get_color()) {
-      rb_node<T> *parent = node->get_parent();
-      rb_node<T> *grandparent = parent->get_parent();
+    while (node->parent() && node->parent()->is_red()) {
+      rb_node<T> *p = node->parent();
+      rb_node<T> *g = p->parent();
 
-      if (parent == grandparent->get_left()) {
-        rb_node<T> *uncle = grandparent->get_right();
-        if (uncle && uncle->get_color()) {
-          parent->change_color();
-          uncle->change_color();
-          grandparent->change_color();
-          node = grandparent;
+      if (p == g->left()) {
+        rb_node<T> *u = g->right();
+        if (u && u->is_red()) {
+          p->toggle_color();
+          u->toggle_color();
+          g->toggle_color();
+          node = g;
         } else {
-          if (node == parent->get_right()) {
-            node = parent;
+          if (node == p->right()) {
+            node = p;
             rotate_left(node);
           }
-          parent->change_color();
-          grandparent->change_color();
-          rotate_right(grandparent);
+          p->toggle_color();
+          g->toggle_color();
+          rotate_right(g);
         }
       } else {
-        rb_node<T> *uncle = grandparent->get_left();
-        if (uncle && uncle->get_color()) {
-          parent->change_color();
-          uncle->change_color();
-          grandparent->change_color();
-          node = grandparent;
+        rb_node<T> *u = g->left();
+        if (u && u->is_red()) {
+          p->toggle_color();
+          u->toggle_color();
+          g->toggle_color();
+          node = g;
         } else {
-          if (node == parent->get_left()) {
-            node = parent;
+          if (node == p->left()) {
+            node = p;
             rotate_right(node);
           }
-          parent->change_color();
-          grandparent->change_color();
-          rotate_left(grandparent);
+          p->toggle_color();
+          g->toggle_color();
+          rotate_left(g);
         }
       }
     }
-    m_root->set_color(false);
+    if (m_root)
+      m_root->set_red(false);
   }
 
 public:
-  explicit rb_tree(Compare cmp_func = [](const T &a,
-                                         const T &b) { return a < b; })
-      : cmp(cmp_func) {}
+  rb_tree() : m_root(nullptr), m_pool_index(0) {}
 
-  void insert(T node) { insert_on_tree(new rb_node<T>(node)); }
+  rb_node<T> *insert(const T &value) {
+    rb_node<T> *new_node = allocate_node(value);
+    if (!new_node)
+      return nullptr; // pool cheio
 
-  void insert_on_tree(rb_node<T> *node) {
     rb_node<T> *y = nullptr;
     rb_node<T> *x = m_root;
 
     while (x) {
       y = x;
-      if (cmp(node->get_value(), x->get_value()))
-        x = x->get_left();
+      if (value < x->value())
+        x = x->left();
       else
-        x = x->get_right();
+        x = x->right();
     }
 
-    node->set_parent(y);
+    new_node->set_parent(y);
+
     if (!y)
-      m_root = node;
-    else if (cmp(node->get_value(), y->get_value()))
-      y->set_left(node);
+      m_root = new_node;
+    else if (value < y->value())
+      y->set_left(new_node);
     else
-      y->set_right(node);
+      y->set_right(new_node);
 
-    node->set_left(nullptr);
-    node->set_right(nullptr);
-    node->set_color(true);
+    new_node->set_left(nullptr);
+    new_node->set_right(nullptr);
+    new_node->set_red(true);
 
-    rebalance(node);
+    rebalance(new_node);
+    return new_node;
   }
 
   rb_node<T> *find(const T &value) const {
     rb_node<T> *current = m_root;
     while (current) {
-      if (cmp(value, current->get_value()))
-        current = current->get_left();
-      else if (cmp(current->get_value(), value))
-        current = current->get_right();
+      if (value < current->value())
+        current = current->left();
+      else if (current->value() < value)
+        current = current->right();
       else
         return current;
     }
     return nullptr;
   }
 
-  rb_node<T> *get_root() const { return m_root; }
+  bool remove(const T &value) {
+    rb_node<T> *z = find(value);
+    if (!z)
+      return false; // não encontrado
+
+    rb_node<T> *y = z;
+    rb_node<T> *x = nullptr;
+    bool y_original_red = y->is_red();
+
+    if (!z->left()) {
+      x = z->right();
+      transplant(z, z->right());
+    } else if (!z->right()) {
+      x = z->left();
+      transplant(z, z->left());
+    } else {
+      y = minimum(z->right());
+      y_original_red = y->is_red();
+      x = y->right();
+
+      if (y->parent() == z) {
+        if (x)
+          x->set_parent(y);
+      } else {
+        transplant(y, y->right());
+        y->set_right(z->right());
+        if (y->right())
+          y->right()->set_parent(y);
+      }
+
+      transplant(z, y);
+      y->set_left(z->left());
+      if (y->left())
+        y->left()->set_parent(y);
+      y->set_red(z->is_red());
+    }
+
+    if (!y_original_red)
+      fix_remove(x);
+
+    return true;
+  }
+
+  rb_node<T> *root() const { return m_root; }
 };
