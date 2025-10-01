cde2020b0b335847c37dbfe08aa04e6b8c56f7b9
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-09-30 22:44:52 -0300

    refactor(physical memory manager): range now need initialize bitmap and page_count

--- Src/Kernel/MemoryManager/PhysicalMemoryManager.cpp
+++ Src/Kernel/MemoryManager/PhysicalMemoryManager.cpp
@@ -56,6 +56,8 @@ void PhysicalMemoryManager::initialize(const multiboot2::TagMemoryMap *mmap) {
         .m_start = entry_start,
         .m_end = entry_end,
         .m_type = type,
+        .m_bitmap = Bitmap<uint64_t, 65535>(),
+        .m_page_count = 0,
     };
 
     if (!m_memory_ranges.insert(range)) {
@@ -70,4 +72,70 @@ void PhysicalMemoryManager::initialize(const multiboot2::TagMemoryMap *mmap) {
 
   is_initialized = true;
   klog("PHYSICAL MEMORY", "Manager initialized with sucess");
+}
+
+int PhysicalMemoryManager::alloc_from_node(rb_node<PhysicalMemoryRange>* node, size_t count, uintptr_t addr_hint){
+  if (!node) {
+    kwarn("PHYSICAL MEMORY", "Node is null");
+    return -1;
+  }
+
+  int index = alloc_from_node(node->left(), count, addr_hint);
+  if (index >= 0){
+    return index;
+  }
+
+  if (node->value().m_type == MemoryType::Usable){
+    index = node->value().alloc_page(count, addr_hint);
+
+    if (index >= 0){
+      return node->value().m_start + index * PAGE_SIZE;
+    }
+  }
+
+  return alloc_from_node(node->right(), count, addr_hint);
+}
+
+void* PhysicalMemoryManager::alloc_physical_page(size_t count, uintptr_t addr_hint){
+  if (!is_initialized) {
+    kwarn("PHYSICAL MEMORY", "Not initialized!");
+    return nullptr;
+  }
+
+  if (count == 0) {
+    kwarn("PHYSICAL MEMORY", "We can't allocate zero pages");
+    return nullptr;
+  }
+
+  uintptr_t addr = alloc_from_node(m_memory_ranges.root(), count, addr_hint);
+  if (!addr){
+    kwarn("PHYSICAL MEMORY", "No more physical memory available");
+    return nullptr;
+  }
+
+  return reinterpret_cast<void*>(addr);
+}
+
+void PhysicalMemoryManager::free_physical_page(void *page){
+  if (!is_initialized){
+    kwarn("PHYSICAL MEMORY", "Not initialized!");
+    return;
+  }
+
+  uintptr_t addr = reinterpret_cast<uintptr_t>(page);
+
+  rb_node<PhysicalMemoryRange>* current = m_memory_ranges.root();
+  while (current){
+    auto &range = current->value();
+    if (addr < range.m_start){
+      current = current->left();
+    } else if (addr >= range.m_end){
+      current = current->right();
+    } else {
+      range.free_page(addr);
+      return;
+    }
+  }
+
+  kwarn("PHYSICAL MEMORY", "The address %p does not belong to any memory range", addr);
 }
