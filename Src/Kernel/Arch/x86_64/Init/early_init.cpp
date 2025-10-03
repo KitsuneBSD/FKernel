5e2cf959fe5e9f5c6b38408276a9f76bbfa327e7
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-10-03 09:37:19 -0300

    refactor(early init): load apic if, only if the CPU supports

--- Src/Kernel/Arch/x86_64/Init/early_init.cpp
+++ Src/Kernel/Arch/x86_64/Init/early_init.cpp
@@ -1,10 +1,16 @@
-#include <Kernel/Boot/multiboot2.h>
-#include <Kernel/Arch/x86_64/Segments/gdt.h>
 #include <Kernel/Arch/x86_64/Interrupt/interrupt_controller.h>
+#include <Kernel/Arch/x86_64/Interrupt/HardwareInterrupts/apic.h>
+#include <Kernel/Arch/x86_64/Segments/gdt.h>
+
+#include <Kernel/Boot/multiboot2.h>
+
+#include <Kernel/Hardware/Cpu.h>
+
 #include <Kernel/MemoryManager/PhysicalMemoryManager.h>
 #include <Kernel/MemoryManager/VirtualMemoryManager.h>
 
 #include <LibFK/Algorithms/log.h>
+
 
 void early_init([[maybe_unused]] const multiboot2::TagMemoryMap *mmap) {
   klog("MULTIBOOT2", "Reference to multiboot2 memory map: %p", mmap);
@@ -13,4 +19,8 @@ void early_init([[maybe_unused]] const multiboot2::TagMemoryMap *mmap) {
   InterruptController::the().initialize();
   PhysicalMemoryManager::the().initialize(mmap);
   VirtualMemoryManager::the().initialize();
+
+  if (CPU::the().has_apic()) {
+    APIC::the().enable();
+  }
 }
