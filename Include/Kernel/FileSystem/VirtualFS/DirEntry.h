28693486f6eeeaa73e521d22839b30920b13bb92
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-10-09 08:47:55 -0300

    docs(vfs): apply a doxygen comments to Inode and DirEntry

--- Include/Kernel/FileSystem/VirtualFS/DirEntry.h
+++ Include/Kernel/FileSystem/VirtualFS/DirEntry.h
@@ -3,13 +3,27 @@
 #include <LibFK/Container/fixed_string.h>
 #include <LibC/stdint.h>
 
+/**
+ * @brief Represents a directory entry in the filesystem.
+ *
+ * A DirEntry associates a filename with an inode number.
+ */
 struct DirEntry
 {
-    fixed_string<256> m_name;
-    uint64_t i_number;
+    fixed_string<256> m_name; ///< Name of the file or directory
+    uint64_t i_number;        ///< Corresponding inode number
 
+    /**
+     * @brief Default constructor. Initializes name to empty and inode number to 0.
+     */
     DirEntry() : m_name(""), i_number(0) {}
 
+    /**
+     * @brief Constructs a DirEntry with a given name and inode number.
+     *
+     * @param name Name of the file or directory
+     * @param inode_num Associated inode number
+     */
     DirEntry(const char *name, uint64_t inode_num)
         : m_name(name), i_number(inode_num) {}
 };
--- Include/Kernel/FileSystem/VirtualFS/Inode.h
+++ Include/Kernel/FileSystem/VirtualFS/Inode.h
@@ -3,17 +3,31 @@
 #include <LibC/stdint.h>
 #include <LibC/stddef.h>
 
+/**
+ * @brief Represents an inode in a filesystem.
+ *
+ * The Inode structure holds metadata about a file or directory,
+ * including its number, permissions, size, timestamps, and pointers
+ * to its data blocks.
+ */
 struct Inode
 {
-    uint64_t i_number;
-    uint32_t permissions;
-    uint64_t size;
-    uint64_t link_count;
-    uint64_t creation_time;
-    uint64_t modification_time;
-    uint64_t access_time;
-    uint64_t data_block_pointers[12];
+    uint64_t i_number;                ///< Unique inode number
+    uint32_t permissions;             ///< File permissions (e.g., read/write/execute)
+    uint64_t size;                    ///< Size of the file in bytes
+    uint64_t link_count;              ///< Number of hard links pointing to this inode
+    uint64_t creation_time;           ///< Time of creation
+    uint64_t modification_time;       ///< Time of last modification
+    uint64_t access_time;             ///< Time of last access
+    uint64_t data_block_pointers[12]; ///< Direct pointers to data blocks (up to 12)
 
+    /**
+     * @brief Constructs a new inode with a given inode number.
+     *
+     * Initializes all metadata fields to default values.
+     *
+     * @param inode_num Unique inode number to assign
+     */
     Inode(uint64_t inode_num)
         : i_number(inode_num), permissions(0), size(0), link_count(1),
           creation_time(0), modification_time(0), access_time(0)
