6c814228db9d5248f8d4a346344e680ea528882c
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-10-12 14:19:34 -0300

    feat(init.cpp): start ramfs as root of FKernel Filesystem

--- Src/Kernel/Init/init.cpp
+++ Src/Kernel/Init/init.cpp
@@ -1,24 +1,24 @@
 #include <Kernel/Boot/init.h>
 #include <LibFK/Algorithms/log.h>
 #include <Kernel/FileSystem/VirtualFS/vfs.h>
+#include <Kernel/FileSystem/RamFS/ramfs.h>
 
 void init()
 {
     auto &vfs = VirtualFS::the();
+    auto &ramfs = RamFS::the();
 
-    RetainPtr<VNode> root = make_retain<VNode>();
-    root->m_name = "/";
-    root->type = VNodeType::Directory;
+    // Monta o RamFS na raiz
+    vfs.mount("/", ramfs.root());
 
-    vfs.mount("/", root);
-
-    RetainPtr<VNode> result;
-    if (vfs.lookup("/", result) == 0)
+    RetainPtr<VNode> root;
+    if (vfs.lookup("/", root) == 0)
     {
-        klog("VFS", "Root lookup success: %s", result->m_name.c_str());
+        klog("VFS", "Root lookup success: %s", root.get()->m_name.c_str());
     }
     else
     {
         kwarn("VFS", "Root lookup failed");
+        return;
     }
 }
