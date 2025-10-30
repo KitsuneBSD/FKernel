cb2ebc5007cc1f12a3fb3d2147dd46a3394a3864
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-10-30 09:07:50 -0300

    feat(long_mode_start): reorder the long_mode_start

--- Src/Kernel/Arch/x86_64/Boot/long_mode_start.asm
+++ Src/Kernel/Arch/x86_64/Boot/long_mode_start.asm
@@ -10,21 +10,21 @@ extern multiboot_info_ptr
 section .text
 bits 64
 long_mode_start:
-  lea rax, [page_table_l4]
-  mov [current_pml4_ptr], rax
-
-  mov eax, [multiboot_magic]
-  mov edi, eax
-  mov rsi, [multiboot_info_ptr]
-  
-  mov rsp, stack_top
-
   mov ax, 0
   mov ss, ax
   mov ds, ax
   mov es, ax
   mov fs, ax
   mov gs, ax
-  
+
+  mov rsp, stack_top
+
+  lea rax, [page_table_l4]
+  mov [current_pml4_ptr], rax
+
+  mov eax, [multiboot_magic]
+  mov edi, eax
+  mov rsi, [multiboot_info_ptr]
+
   call kmain
   hlt
