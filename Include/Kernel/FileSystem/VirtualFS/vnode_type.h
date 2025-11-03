0b2e195444f4016c4e079dffc3eafd810a074589
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-11-03 16:06:31 -0300

    refactor(libfk): use types/types.h instead LibC

--- Include/Kernel/FileSystem/VirtualFS/vnode_type.h
+++ Include/Kernel/FileSystem/VirtualFS/vnode_type.h
@@ -1,18 +1,18 @@
 #pragma once
-#include <LibC/stdint.h>
+
+#include <LibFK/Types/types.h>
 
 /**
  * @brief Enum representing the type of a vnode
  */
 
-enum class VNodeType : uint8_t
-{
-    Regular,
-    Directory,
-    Symlink,
-    CharacterDevice,
-    BlockDevice,
-    Socket,
-    FIFO,
-    Unknown
+enum class VNodeType : uint8_t {
+  Regular,
+  Directory,
+  Symlink,
+  CharacterDevice,
+  BlockDevice,
+  Socket,
+  FIFO,
+  Unknown
 };
