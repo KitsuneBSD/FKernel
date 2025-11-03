f83be80034283e72d36fdf955da5bb264b87ed46
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-11-03 15:51:15 -0300

    docs(ata): use doxygen

--- Include/Kernel/Driver/Ata/AtaBlockCache.h
+++ Include/Kernel/Driver/Ata/AtaBlockCache.h
@@ -1,27 +1,56 @@
 #pragma once
 
-#include <LibC/stdint.h>
-
 #include <Kernel/Driver/Ata/AtaController.h>
+#include <LibFK/Types/types.h>
 
-struct SectorCacheEntry
-{
-    uint32_t lba;
-    bool is_valid;
-    bool is_dirty;
-    uint8_t data[512];
+/**
+ * @brief Represents a single cached sector in the ATA cache
+ */
+struct SectorCacheEntry {
+    uint32_t lba;      ///< LBA (Logical Block Address) of the cached sector
+    bool is_valid;     ///< Whether this cache entry contains valid data
+    bool is_dirty;     ///< Whether this sector has been modified and needs flushing
+    uint8_t data[512]; ///< Sector data (typically 512 bytes)
 };
 
-class AtaCache
-{
+/**
+ * @brief Simple sector cache for ATA devices
+ * 
+ * Provides caching for up to 8 sectors to reduce physical ATA reads/writes.
+ * Implements a singleton pattern for global access.
+ */
+class AtaCache {
 private:
-    AtaCache() = default;
-    SectorCacheEntry cache[8];
+    AtaCache() = default;          ///< Private constructor for singleton
+    SectorCacheEntry cache[8];     ///< Fixed-size sector cache
 
 public:
+    /**
+     * @brief Get the singleton instance of AtaCache
+     * @return Reference to the global AtaCache
+     */
     static AtaCache &the();
 
+    /**
+     * @brief Retrieve a sector from the cache or read it from the device
+     * 
+     * @param device Pointer to the ATA device
+     * @param lba Logical Block Address of the sector
+     * @return Pointer to the cached sector data
+     */
     uint8_t *get_sector(AtaDeviceInfo *device, uint32_t lba);
+
+    /**
+     * @brief Mark a cached sector as dirty
+     * 
+     * @param lba Logical Block Address of the sector
+     */
     void mark_dirty(uint32_t lba);
+
+    /**
+     * @brief Flush all dirty sectors to the physical ATA device
+     * 
+     * @param device Pointer to the ATA device
+     */
     void flush(AtaDeviceInfo *device);
 };
--- Include/Kernel/Driver/Ata/AtaBlockDevice.h
+++ Include/Kernel/Driver/Ata/AtaBlockDevice.h
@@ -1,15 +1,62 @@
 #pragma once
 
-#include <Kernel/FileSystem/VirtualFS/vnode_ops.h>
 #include <Kernel/FileSystem/VirtualFS/vnode.h>
+#include <Kernel/FileSystem/VirtualFS/vnode_ops.h>
 #include <Kernel/FileSystem/file_descriptor.h>
 
-struct AtaBlockDevice
-{
-    static int open(VNode *vnode, FileDescriptor *fd, int flags);
-    static int close(VNode *vnode, FileDescriptor *fd);
-    static int read(VNode *vnode, FileDescriptor *fd, void *buffer, size_t size, size_t offset);
-    static int write(VNode *vnode, FileDescriptor *fd, const void *buffer, size_t size, size_t offset);
+/**
+ * @brief Block device interface for ATA devices
+ *
+ * Provides static methods compatible with the Virtual File System (VFS)
+ * to perform operations on a physical ATA device, including open, close,
+ * read, and write.
+ */
+struct AtaBlockDevice {
+  /**
+   * @brief Open an ATA device
+   *
+   * @param vnode VNode representing this device
+   * @param fd File descriptor
+   * @param flags Open flags
+   * @return 0 on success, negative error code on failure
+   */
+  static int open(VNode *vnode, FileDescriptor *fd, int flags);
 
-    static VNodeOps ops;
+  /**
+   * @brief Close an ATA device
+   *
+   * @param vnode VNode representing this device
+   * @param fd File descriptor
+   * @return 0 on success, negative error code on failure
+   */
+  static int close(VNode *vnode, FileDescriptor *fd);
+
+  /**
+   * @brief Read data from the ATA device
+   *
+   * @param vnode VNode representing this device
+   * @param fd File descriptor
+   * @param buffer Pointer to the buffer where data will be stored
+   * @param size Number of bytes to read
+   * @param offset Offset in the device to start reading from
+   * @return Number of bytes read on success, negative error code on failure
+   */
+  static int read(VNode *vnode, FileDescriptor *fd, void *buffer, size_t size,
+                  size_t offset);
+
+  /**
+   * @brief Write data to the ATA device
+   *
+   * @param vnode VNode representing this device
+   * @param fd File descriptor
+   * @param buffer Pointer to the data to write
+   * @param size Number of bytes to write
+   * @param offset Offset in the device to start writing to
+   * @return Number of bytes written on success, negative error code on failure
+   */
+  static int write(VNode *vnode, FileDescriptor *fd, const void *buffer,
+                   size_t size, size_t offset);
+
+  /// Virtual node operations table for this block device
+  static VNodeOps ops;
 };
