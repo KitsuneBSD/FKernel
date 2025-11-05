043b0d0bc518705fcae3f62e808b5ff49ca53a08
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-11-05 19:23:07 -0300

    refactor(interrupt): increase lot of info on exceptions

--- Src/Kernel/Arch/x86_64/Interrupt/Handler/gp_handler.cpp
+++ Src/Kernel/Arch/x86_64/Interrupt/Handler/gp_handler.cpp
@@ -4,11 +4,19 @@
 
 void general_protection_handler([[maybe_unused]] uint8_t vector,
                                 InterruptFrame *frame) {
-  kexception("General Protection", "General protection interrupt: vector=%u\n",
+  kexception("General Protection", "General protection interrupt: vector=%u",
              (unsigned)vector);
   kexception("General Protection", "RIP=%p CS=%p RFLAGS=%p", frame->rip,
              frame->cs, frame->rflags);
   kexception("General Protection", "RSP=%p SS=%p", frame->rsp, frame->ss);
+  kexception("General Protection", "RAX=%p RBX=%p RCX=%p RDX=%p", frame->rax,
+             frame->rbx, frame->rcx, frame->rdx);
+  kexception("General Protection", "RSI=%p RDI=%p RBP=%p", frame->rsi,
+             frame->rdi, frame->rbp);
+  kexception("General Protection", "R8=%p R9=%p R10=%p R11=%p", frame->r8,
+             frame->r9, frame->r10, frame->r11);
+  kexception("General Protection", "R12=%p R13=%p R14=%p R15=%p", frame->r12,
+             frame->r13, frame->r14, frame->r15);
 
   for (;;) {
     asm volatile("cli;hlt");
