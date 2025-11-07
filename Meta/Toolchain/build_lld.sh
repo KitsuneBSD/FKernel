67f05cc720a0a219f8b256fd27306a36f5ab3d94
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-11-07 09:43:50 -0300

    chore(meta): add function to compile clang and lld

---
+++ Meta/Toolchain/build_lld.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+set -e
+
+if command -v nproc >/dev/null 2>&1; then
+	CORES=$(nproc)
+elif command -v sysctl >/dev/null 2>&1; then
+	CORES=$(sysctl -n hw.ncpu)
+else
+	CORES=1
+fi
+echo "Detected $CORES cores"
+
+mkdir -p Toolchain
+mkdir -p build
+
+if ! command -v cmake >/dev/null 2>&1; then
+	echo "cmake not found. Please install it before continuing."
+	exit 1
+fi
+
+if ! command -v ninja >/dev/null 2>&1; then
+	echo "ninja not found. Please install it before continuing."
+	exit 1
+fi
+
+if ! command -v git >/dev/null 2>&1; then
+	echo "git not found. Please install it before continuing."
+	exit 1
+fi
+
+if ! command -v make >/dev/null 2>&1; then
+	echo "make not found. Please install it before continuing."
+	exit 1
+fi
+
+if [ ! -d "build/llvm" ]; then
+	git clone https://github.com/llvm/llvm-project.git -b release/21.x build/llvm
+fi
+
+cd build/llvm
+mkdir build && cd build
+cmake -G Ninja ../llvm \
+	-DLLVM_ENABLE_PROJECTS="lld" \
+	-DLLVM_TARGETS_TO_BUILD="X86" \
+	-DLLVM_ENABLE_TERMINFO=OFF \
+	-DLLVM_ENABLE_LIBEDIT=OFF \
+	-DLLVM_ENABLE_ZLIB=OFF \
+	-DLLVM_ENABLE_ZSTD=OFF \
+	-DLLVM_INCLUDE_TESTS=OFF \
+	-DLLVM_INCLUDE_EXAMPLES=OFF \
+	-DLLVM_INCLUDE_BENCHMARKS=OFF \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX="$PWD/Toolchain/" \
+	-DLLVM_DEFAULT_TARGET_TRIPLE="x86_64-unknown-elf" \
+	-DCLANG_DEFAULT_LINKER=lld \
+	-DCLANG_DEFAULT_RTLIB=compiler-rt \
+	-DCLANG_DEFAULT_CXX_STDLIB=libc++ \
+	-DLLVM_ENABLE_RUNTIMES=""
+
+ninja -j"$CORES" clang
+ninja install
+
+mv "$PWD/Toolchain/lld" "$PWD/Toolchain/fkernel-lld"
+
+echo "Lld successfully compiled and moved to Toolchain!"
