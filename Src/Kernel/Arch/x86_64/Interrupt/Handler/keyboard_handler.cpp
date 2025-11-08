eaa32c8225b0cf7263c4d4a223751f36f383dd03
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-11-08 10:32:39 -0300

    feat(interruptcontroller): apply all x86_64 interrupts

--- Src/Kernel/Arch/x86_64/Interrupt/Handler/keyboard_handler.cpp
+++
@@ -1,9 +0,0 @@
-#include <Kernel/Arch/x86_64/Interrupt/Handler/handlers.h>
-#include <Kernel/Driver/Keyboard/ps2_keyboard.h>
-
-void keyboard_handler(uint8_t vector, InterruptFrame *frame)
-{
-    (void)vector;
-    (void)frame;
-    PS2Keyboard::the().irq_handler();
-}
