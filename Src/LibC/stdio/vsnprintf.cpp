89ea8023800bf1003a1b8f69492191d2b640630f
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-11-13 17:20:38 -0300

    feat(stdio): add support to zero-padding and width

--- Src/LibC/stdio/vsnprintf.cpp
+++ Src/LibC/stdio/vsnprintf.cpp
@@ -1,18 +1,36 @@
 #include <LibC/stdarg.h>
-#include <LibC/stdio.h>
 #include <LibC/string.h>
+#include <LibFK/Types/types.h>
 
 extern "C" int vsnprintf(char *buf, size_t size, const char *fmt,
                          va_list args) {
   size_t pos = 0;
 
+  auto write_char = [&](char c) {
+    if (pos + 1 < size)
+      buf[pos++] = c;
+  };
+
   for (size_t i = 0; fmt[i] && pos + 1 < size; i++) {
     if (fmt[i] != '%') {
-      buf[pos++] = fmt[i];
+      write_char(fmt[i]);
       continue;
     }
 
     i++;
+    char pad_char = ' ';
+    int width = 0;
+
+    if (fmt[i] == '0') {
+      pad_char = '0';
+      i++;
+    }
+
+    while (fmt[i] >= '0' && fmt[i] <= '9') {
+      width = width * 10 + (fmt[i] - '0');
+      i++;
+    }
+
     bool is_long = false;
     bool is_size_t = false;
 
@@ -24,82 +42,75 @@ extern "C" int vsnprintf(char *buf, size_t size, const char *fmt,
       i++;
     }
 
+    char spec = fmt[i];
     char temp[65];
-    switch (fmt[i]) {
+    temp[0] = '\0';
+
+    switch (spec) {
     case 'c': {
       char c = (char)va_arg(args, int);
-      if (pos < size - 1)
-        buf[pos++] = c;
+      temp[0] = c;
+      temp[1] = '\0';
       break;
     }
     case 's': {
       const char *s = va_arg(args, const char *);
-      while (*s && pos + 1 < size)
-        buf[pos++] = *s++;
+      strncpy(temp, s, sizeof(temp) - 1);
+      temp[sizeof(temp) - 1] = '\0';
       break;
     }
     case 'd': {
-      if (is_long) {
-        long val = va_arg(args, long);
-        itoa_signed(val, temp, 10);
-      } else {
-        int val = va_arg(args, int);
-        itoa_signed(val, temp, 10);
-      }
-      for (char *p = temp; *p && pos + 1 < size; p++)
-        buf[pos++] = *p;
+      long val = is_long ? va_arg(args, long) : va_arg(args, int);
+      itoa_signed(val, temp, 10);
       break;
     }
     case 'u': {
-      if (is_long) {
-        unsigned long val = va_arg(args, unsigned long);
-        itoa_unsigned(val, temp, 10, false);
-      } else if (is_size_t) {
-        size_t val = va_arg(args, size_t);
-        itoa_size(val, temp, 10);
-      } else {
-        unsigned int val = va_arg(args, unsigned int);
-        itoa_unsigned(val, temp, 10, false);
-      }
-      for (char *p = temp; *p && pos + 1 < size; p++)
-        buf[pos++] = *p;
+      unsigned long val;
+      if (is_long)
+        val = va_arg(args, unsigned long);
+      else if (is_size_t)
+        val = va_arg(args, size_t);
+      else
+        val = va_arg(args, unsigned int);
+      itoa_unsigned(val, temp, 10, false);
       break;
     }
     case 'x': {
-      if (is_long) {
-        unsigned long val = va_arg(args, unsigned long);
-        itoa_unsigned(val, temp, 16, false);
-      } else if (is_size_t) {
-        size_t val = va_arg(args, size_t);
-        itoa_size(val, temp, 16);
-      } else {
-        unsigned int val = va_arg(args, unsigned int);
-        itoa_unsigned(val, temp, 16, false);
-      }
-      for (char *p = temp; *p && pos + 1 < size; p++)
-        buf[pos++] = *p;
+      unsigned long val;
+      if (is_long)
+        val = va_arg(args, unsigned long);
+      else if (is_size_t)
+        val = va_arg(args, size_t);
+      else
+        val = va_arg(args, unsigned int);
+      itoa_unsigned(val, temp, 16, false);
       break;
     }
     case 'p': {
       uintptr_t val = (uintptr_t)va_arg(args, void *);
       itoa_unsigned(val, temp, 16, false);
-      if (pos + 2 < size)
-        buf[pos++] = '0';
-      if (pos + 2 < size)
-        buf[pos++] = 'x';
-      for (char *p = temp; *p && pos + 1 < size; p++)
-        buf[pos++] = *p;
       break;
     }
-    case '%':
-      buf[pos++] = '%';
-      break;
-    default:
-      buf[pos++] = '%';
-      if (pos + 1 < size)
-        buf[pos++] = fmt[i];
+    case '%': {
+      temp[0] = '%';
+      temp[1] = '\0';
       break;
     }
+    default: {
+      temp[0] = '%';
+      temp[1] = spec;
+      temp[2] = '\0';
+      break;
+    }
+    }
+
+    size_t len = strlen(temp);
+    int padding = (width > (int)len) ? (width - (int)len) : 0;
+    for (int p = 0; p < padding && pos + 1 < size; p++)
+      write_char(pad_char);
+
+    for (size_t p = 0; p < len && pos + 1 < size; p++)
+      write_char(temp[p]);
   }
 
   buf[pos] = '\0';
