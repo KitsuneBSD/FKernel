0c2a1b3024f01a2154ba64b0eeb56cf4147dda7b
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-12-11 07:49:38 -0300

    feat(libfk): create a header with a utilities from align items

---
+++ Include/LibFK/Utilities/aligner.h
@@ -0,0 +1,18 @@
+#pragma once 
+
+#include <LibFK/Types/types.h>
+
+template <typename T>
+constexpr T align_up(T value, size_t alignment) {
+    return (value + alignment - 1) & ~(alignment - 1);
+}
+
+template <typename T>
+constexpr T align_down(T value, size_t alignment) {
+    return value & ~(alignment - 1);
+}
+
+template <typename T>
+constexpr bool is_aligned(T value, size_t alignment) {
+    return (value & (alignment - 1)) == 0;
+}
