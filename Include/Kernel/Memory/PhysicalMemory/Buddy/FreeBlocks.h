c3f1d6fc75be8bdb9925941749a522b346e68c44
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-12-11 08:20:41 -0300

    refactor(freeblock): freeBlock now is a simple linked list

--- Include/Kernel/Memory/PhysicalMemory/Buddy/FreeBlocks.h
+++ Include/Kernel/Memory/PhysicalMemory/Buddy/FreeBlocks.h
@@ -1,14 +1,8 @@
 #pragma once 
 
-/* 
-* Usually, a double linked list can be used to easily coalesce free blocks in buddy memory allocation.
-* Can link on previous block and a next block
-*/
-
 /**
  * @brief Represents a free block in the buddy memory allocator.
  */
 struct FreeBlock {
-    FreeBlock* prev;    
     FreeBlock* next;
 };
