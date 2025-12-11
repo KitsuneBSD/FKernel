380751c7fecfae4ead576c2964906a68082f54b7
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-12-11 08:34:11 -0300

    refactor(arch defs): page size is exclusively defined on BuddyOrder.h

--- Include/Kernel/Arch/x86_64/arch_defs.h
+++ Include/Kernel/Arch/x86_64/arch_defs.h
@@ -8,17 +8,6 @@
 constexpr uint8_t MAX_X86_64_INTERRUPTS_LENGTH = 256;
 
 /**
- * @brief Standard page size (4 KiB)
- */
-constexpr size_t PAGE_SIZE = 4 * fk::types::KiB;
-constexpr uintptr_t PAGE_SIZE_2M = 2 * fk::types::MiB;
-
-/**
- * @brief Mask to align addresses to the 4 KiB page boundary
- */
-constexpr size_t PAGE_MASK = (~(PAGE_SIZE - 1));
-
-/**
  * @brief Maximum number of memory chunks per range
  */
 constexpr size_t MAX_CHUNKS_PER_RANGE = UINT64_MAX;
