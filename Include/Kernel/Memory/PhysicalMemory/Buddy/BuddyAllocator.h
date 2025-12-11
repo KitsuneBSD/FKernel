7171643d794ec7664c95edb6f0eff481371cc067
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-12-11 12:14:34 -0300

    refactor(physicalmemorymanager): add logs on instance of objects

--- Include/Kernel/Memory/PhysicalMemory/Buddy/BuddyAllocator.h
+++ Include/Kernel/Memory/PhysicalMemory/Buddy/BuddyAllocator.h
@@ -1,60 +1,61 @@
-#pragma once 
+#pragma once
 
-#include <LibFK/Types/types.h>
-#include <LibFK/Container/string.h>
-#include <LibFK/Utilities/aligner.h>
-#include <Kernel/Memory/PhysicalMemory/Buddy/FreeBlocks.h>
-#include <Kernel/Memory/PhysicalMemory/Buddy/BuddyOrder.h>
+#include "LibFK/Algorithms/log.h"
 #include <Kernel/Arch/x86_64/arch_defs.h>
+#include <Kernel/Memory/PhysicalMemory/Buddy/BuddyOrder.h>
+#include <Kernel/Memory/PhysicalMemory/Buddy/FreeBlocks.h>
+#include <LibFK/Container/string.h>
+#include <LibFK/Types/types.h>
+#include <LibFK/Utilities/aligner.h>
 
 class BuddyAllocator {
 private:
-    FreeBlock m_block_pool[16384];
-    size_t m_block_index = 0;
-    uintptr_t m_base_address; 
-    size_t m_length;
+  FreeBlock m_block_pool[16384];
+  size_t m_block_index = 0;
+  uintptr_t m_base_address;
+  size_t m_length;
+
 protected:
-    void initialize();
-    
-    size_t order_to_index(size_t order) const; 
-    uintptr_t block_address(void* ptr) const;
-    size_t largest_order_fit(size_t size) const; 
-    uintptr_t buddy_of(uintptr_t address, size_t order) const; 
-    bool in_range(uintptr_t address) const; 
-    void push_free_block(size_t order, uintptr_t address);
-    uintptr_t pop_free_block(size_t order);
+  void initialize();
 
-    FreeBlock* new_block(uintptr_t phys) {
-        FreeBlock* b = &m_block_pool[m_block_index++];
-        b->phys_addr = phys;
-        b->next = nullptr;
-        return b;
-    }
+  size_t order_to_index(size_t order) const;
+  uintptr_t block_address(void *ptr) const;
+  size_t largest_order_fit(size_t size) const;
+  uintptr_t buddy_of(uintptr_t address, size_t order) const;
+  bool in_range(uintptr_t address) const;
+  void push_free_block(size_t order, uintptr_t address);
+  uintptr_t pop_free_block(size_t order);
 
-public: 
-    BuddyAllocator()
-        : m_base_address(0)
-        , m_length(0)
-    {
-    }
+  FreeBlock *new_block(uintptr_t phys) {
+    FreeBlock *b = &m_block_pool[m_block_index++];
+    b->phys_addr = phys;
+    b->next = nullptr;
+    return b;
+  }
 
-    BuddyAllocator(uintptr_t base_address, size_t length) 
-        : m_base_address(base_address)
-        , m_length(length)
-    {
-        initialize();
-    }
+public:
+  BuddyAllocator() : m_base_address(0), m_length(0) {
+    fk::algorithms::kdebug("BUDDY", "Buddy allocator created");
+  }
 
-    void add_range(uintptr_t base_address, size_t length){
-        m_base_address = base_address;
-        m_length = length;
+  BuddyAllocator(uintptr_t base_address, size_t length)
+      : m_base_address(base_address), m_length(length) {
+    fk::algorithms::kdebug("BUDDY", "Buddy allocator created");
+    initialize();
+  }
 
-        initialize();
-    }
+  void add_range(uintptr_t base_address, size_t length) {
+    m_base_address = base_address;
+    m_length = length;
 
-    void* alloc(size_t order);
-    void free(void* ptr, size_t order);
+    fk::algorithms::kdebug("BUDDY", "Add a new range with %p on %lu",
+                           m_base_address, m_length);
+    initialize();
+  }
 
-    uintptr_t base_address() const { return m_base_address; }
-    size_t length() const { return m_length; }
+  void *alloc(size_t order);
+  void free(void *ptr, size_t order);
+
+  uintptr_t base_address() const { return m_base_address; }
+  size_t length() const { return m_length; }
 };
--- Include/Kernel/Memory/PhysicalMemory/Zone/ZoneAllocator.h
+++ Include/Kernel/Memory/PhysicalMemory/Zone/ZoneAllocator.h
@@ -1,34 +1,42 @@
-#pragma once 
+#pragma once
 
+#include "LibFK/Algorithms/log.h"
+#include <Kernel/Memory/PhysicalMemory/Zone/ZoneDefs.h>
+#include <Kernel/Memory/PhysicalMemory/Zone/ZoneType.h>
 #include <LibFK/Container/bitmap.h>
 #include <LibFK/Types/types.h>
-#include <Kernel/Memory/PhysicalMemory/Zone/ZoneType.h>
-#include <Kernel/Memory/PhysicalMemory/Zone/ZoneDefs.h>
 
 class Zone {
 private:
-    uintptr_t m_base {0}; 
-    size_t m_length {0};
-    size_t m_frame_count {0};
+  uintptr_t m_base{0};
+  size_t m_length{0};
+  size_t m_frame_count{0};
 
-    ZoneType m_type { ZoneType::NORMAL };
-    fk::containers::Bitmap<uint64_t, BITMAP_SIZE> m_bitmap;
+  ZoneType m_type{ZoneType::NORMAL};
+  fk::containers::Bitmap<uint64_t, BITMAP_SIZE> m_bitmap;
+
 public:
-    Zone() = default;
-    Zone(uintptr_t base, size_t length, ZoneType type) : m_base(base), m_length(length), m_frame_count(length / FRAME_SIZE), m_type(type), m_bitmap(m_frame_count) {
-        m_bitmap.clear_all();
-    }
+  Zone() = default;
+  Zone(uintptr_t base, size_t length, ZoneType type)
+      : m_base(base), m_length(length), m_frame_count(length / FRAME_SIZE),
+        m_type(type), m_bitmap(m_frame_count) {
+    fk::algorithms::kdebug(
+        "ZONE",
+        "Create a new Zone instance with %lu base on %lu size on %d type", base,
+        length, type);
+    m_bitmap.clear_all();
+  }
 
-    uintptr_t base() const { return m_base; }
-    size_t length() const { return m_length; }
-    ZoneType type() const { return m_type; }
-    size_t frame_count() const { return m_frame_count; }
+  uintptr_t base() const { return m_base; }
+  size_t length() const { return m_length; }
+  ZoneType type() const { return m_type; }
+  size_t frame_count() const { return m_frame_count; }
 
-    void mark_used(size_t index, size_t count = 1);
-    void mark_free(size_t index, size_t count = 1);
+  void mark_used(size_t index, size_t count = 1);
+  void mark_free(size_t index, size_t count = 1);
 
-    uintptr_t alloc_frame();
-    void free_frame(uintptr_t addr);
+  uintptr_t alloc_frame();
+  void free_frame(uintptr_t addr);
 
-    bool is_free(size_t index ) const;
+  bool is_free(size_t index) const;
 };
