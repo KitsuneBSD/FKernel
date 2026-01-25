a8c36e38deeff4a9d8f9514448b12b374c5378b3
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2026-01-25 11:37:50 -0300

    refactor(clock): increase the clock depth to easy expand the clock

---
+++ Include/Kernel/Clock/Types/Datetime.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <LibFK/Core/Result.h>
+#include <LibFK/Text/string.h>
+#include <LibFK/Types/types.h>
+
+struct DateTime {
+  uint8_t second;
+  uint8_t minute;
+  uint8_t hour;
+  uint8_t day;
+  uint8_t month;
+  uint16_t year;
+
+  fk::text::String to_string() const;
+  void print();
+};
