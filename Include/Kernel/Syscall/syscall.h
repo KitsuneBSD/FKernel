814d9a1322b02097ccf3bbfc95c5bfa7c5529554
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2026-01-26 23:16:55 -0300

    fix(kernel/syscall): synchronize syscall declarations in syscall.h
    - Updated all syscall declarations in Include/Kernel/Syscall/syscall.h to include the PtRegs* regs parameter.
    - This resolves 'conflicting types' compilation errors between declarations and definitions.

--- Include/Kernel/Syscall/syscall.h
+++ Include/Kernel/Syscall/syscall.h
@@ -36,29 +36,29 @@ uint64_t sys_fork(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4,
 uint64_t sys_open(uint64_t path, uint64_t flags, uint64_t arg3, uint64_t arg4,
                   uint64_t arg5, uint64_t arg6, PtRegs* regs);
 uint64_t sys_close(uint64_t fd, uint64_t arg2, uint64_t arg3, uint64_t arg4,
-                   uint64_t arg5, uint64_t arg6);
+                   uint64_t arg5, uint64_t arg6, PtRegs* regs);
 uint64_t sys_read(uint64_t fd, uint64_t buf, uint64_t count, uint64_t arg4,
-                  uint64_t arg5, uint64_t arg6);
+                  uint64_t arg5, uint64_t arg6, PtRegs* regs);
 uint64_t sys_write(uint64_t fd, uint64_t buf, uint64_t count, uint64_t arg4,
-                   uint64_t arg5, uint64_t arg6);
+                   uint64_t arg5, uint64_t arg6, PtRegs* regs);
 uint64_t sys_lseek(uint64_t fd, uint64_t offset, uint64_t whence, uint64_t arg4,
-                   uint64_t arg5, uint64_t arg6);
+                   uint64_t arg5, uint64_t arg6, PtRegs* regs);
 uint64_t sys_exit(uint64_t status, uint64_t arg2, uint64_t arg3, uint64_t arg4,
-                  uint64_t arg5, uint64_t arg6);
+                  uint64_t arg5, uint64_t arg6, PtRegs* regs);
 uint64_t sys_getuid(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4,
-                   uint64_t arg5, uint64_t arg6);
+                   uint64_t arg5, uint64_t arg6, PtRegs* regs);
 uint64_t sys_geteuid(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4,
-                    uint64_t arg5, uint64_t arg6);
+                    uint64_t arg5, uint64_t arg6, PtRegs* regs);
 uint64_t sys_getgid(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4,
-                   uint64_t arg5, uint64_t arg6);
+                   uint64_t arg5, uint64_t arg6, PtRegs* regs);
 uint64_t sys_getegid(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4,
-                    uint64_t arg5, uint64_t arg6);
+                    uint64_t arg5, uint64_t arg6, PtRegs* regs);
 uint64_t sys_getpgrp(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4,
-                    uint64_t arg5, uint64_t arg6);
+                    uint64_t arg5, uint64_t arg6, PtRegs* regs);
 uint64_t sys_yield(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4,
-                   uint64_t arg5, uint64_t arg6);
+                   uint64_t arg5, uint64_t arg6, PtRegs* regs);
 uint64_t sys_fcntl(uint64_t fd, uint64_t cmd, uint64_t arg, uint64_t arg4,
-                   uint64_t arg5, uint64_t arg6);
+                   uint64_t arg5, uint64_t arg6, PtRegs* regs);
 
 void initialize_syscalls();
 
